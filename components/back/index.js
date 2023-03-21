c4b8aba44e73fb921e83102e25562e12afbf5f53
knh6269 <knh6269@gmail.com>
2023-03-21 23:52:18 +0900

    feat : add back components (#3) 뒤로가기 컴포넌트 개발

---
+++ components/back/index.js
@@ -0,0 +1,13 @@
+import { BackComponent } from "./style"
+
+export default function Back(){
+    return (
+        <>
+            <BackComponent>
+                <img src = 'https://user-images.githubusercontent.com/44117975/226276985-3b1e269e-bf2a-470b-9c65-953b62149fbd.svg'
+                     alt = 'back-arrow'/>
+            </BackComponent>
+            
+        </>
+    )
+}
