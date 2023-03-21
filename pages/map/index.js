549b058b7f239b496b211548df2b0a84be05a6e0
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-21 15:37:48 +0900

    feat : update map/index.js (#5) style 속성 수정

--- pages/map/index.js
+++ pages/map/index.js
@@ -3,11 +3,10 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk'
 
 const KaKaoMap = () => {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <Map
-        center={{ lat: 33.5563, lng: 126.79581 }}
-        style={{ width: '100%', height: '100%' }}
-      ></Map>
+    <div style={{ height: '100vh' }}>
+      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ height: '100%' }}>
+        <button>새로고침</button>
+      </Map>
     </div>
   )
 }
