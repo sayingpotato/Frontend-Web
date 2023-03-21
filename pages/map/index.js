49f006287910ee89c774e99c6a0f4d63db0648e4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-21 16:09:20 +0900

    feat : update map/index.js (#5) 현재 위치의 위도, 경도를 중심으로 지도 생성

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,12 +1,31 @@
-import { style } from './style.js'
+import { useEffect, useState } from 'react'
 import { Map, MapMarker } from 'react-kakao-maps-sdk'
 
+import { style } from './style.js'
+import currentLocation from '../../utils/getCurrentLocation'
+
 const KaKaoMap = () => {
+  const [location, setLocation] = useState(null)
+  const [lat, setLet] = useState(36.1)
+  const [lng, setLng] = useState(36.1)
+
+  useEffect(() => {
+    currentLocation()
+      .then((result) => {
+        setLocation(result)
+        setLet(result.latitude)
+        setLng(result.longitude)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [])
   return (
     <div style={{ height: '100vh' }}>
-      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ height: '100%' }}>
-        <button>새로고침</button>
-      </Map>
+      <Map
+        center={{ lat: `${lat}`, lng: `${lng}` }}
+        style={{ height: '100%' }}
+      ></Map>
     </div>
   )
 }
