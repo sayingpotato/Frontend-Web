c784c1cbf760b2d07285cc216994897f1351bff2
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-22 18:31:28 +0900

    feat : update map/index.js (#7) 상태 (영업 중, 영업 종료)에 따른 마커 다르게 표현하기

--- pages/map/index.js
+++ pages/map/index.js
@@ -13,14 +13,54 @@ const KaKaoMap = () => {
       lat: 36.6283,
       lng: 127.456,
       name: '우체국',
+      status: 1,
     },
     {
       id: 2,
       lat: 36.628,
       lng: 127.453,
       name: '병원',
+      status: 0,
+    },
+    {
+      id: 3,
+      lat: 36.638,
+      lng: 127.456,
+      name: '우체국2',
+      status: 1,
     },
   ]
+
+  const result = data.map((d) => {
+    if (d.status === 1) {
+      return (
+        <MapMarker
+          key={`${d.key}`}
+          position={{ lat: `${d.lat}`, lng: `${d.lng}` }}
+          image={{
+            src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+            size: {
+              width: 64,
+              height: 69,
+            },
+            options: {
+              offset: {
+                x: 27,
+                y: 69,
+              },
+            },
+          }}
+        ></MapMarker>
+      )
+    } else {
+      return (
+        <MapMarker
+          key={`${d.key}`}
+          position={{ lat: `${d.lat}`, lng: `${d.lng}` }}
+        ></MapMarker>
+      )
+    }
+  })
 
   const [lat, setLat] = useState(0)
   const [lng, setLng] = useState(0)
@@ -57,12 +97,7 @@ const KaKaoMap = () => {
   return (
     <StyledMapDiv>
       <StyledMap center={state.center} ref={mapRef}>
-        {data.map((d) => (
-          <MapMarker
-            key={`${d.key}`}
-            position={{ lat: `${d.lat}`, lng: `${d.lng}` }}
-          ></MapMarker>
-        ))}
+        {result}
         <StyledMapButton onClick={refreshButtonClick}>
           <BiCurrentLocation size={40} />
         </StyledMapButton>
