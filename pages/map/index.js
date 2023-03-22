4f17fc8dbd594f81069974fe6f8a7768b8c82119
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-22 15:02:39 +0900

    feat : update map/index.js (#5) 새로 고침 버튼 활성화

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,31 +1,47 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Map, MapMarker } from 'react-kakao-maps-sdk'
 
-import { style } from './style.js'
 import currentLocation from '../../utils/getCurrentLocation'
 
 const KaKaoMap = () => {
-  const [location, setLocation] = useState(null)
-  const [lat, setLet] = useState(36.1)
-  const [lng, setLng] = useState(36.1)
+  const [lat, setLat] = useState(0)
+  const [lng, setLng] = useState(0)
+
+  const [state, setState] = useState({
+    center: { lat: lat, lng: lng },
+  })
+  const mapRef = useRef()
 
   useEffect(() => {
     currentLocation()
       .then((result) => {
-        setLocation(result)
-        setLet(result.latitude)
+        setLat(result.latitude)
         setLng(result.longitude)
+        setState({
+          center: { lat: result.latitude, lng: result.longitude },
+        })
       })
       .catch((error) => {
         console.error(error)
       })
-  }, [])
+  }, [state])
+
+  const refreshButtonClick = () => {
+    const map = mapRef.current
+    let clat = map.getCenter().getLat()
+    let clng = map.getCenter().getLng()
+
+    setState({
+      center: { lat: clat, lng: clng },
+    })
+  }
+
   return (
     <div style={{ height: '100vh' }}>
-      <Map
-        center={{ lat: `${lat}`, lng: `${lng}` }}
-        style={{ height: '100%' }}
-      ></Map>
+      <button onClick={refreshButtonClick}>Refresh</button>
+      <Map center={state.center} style={{ height: '100%' }} ref={mapRef}>
+        <button onClick={refreshButtonClick}>Refresh</button>
+      </Map>
     </div>
   )
 }
