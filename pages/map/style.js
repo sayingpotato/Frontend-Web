e0d45113c15e6175cba1eb726bc582b0e77f934d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-22 15:44:32 +0900

    feat : update map/style.js (#5) styled-component로 스타일 작성

--- pages/map/style.js
+++ pages/map/style.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components'
+import { Map } from 'react-kakao-maps-sdk'
 
-export {}
+const StyledMapDiv = styled.div`
+  height: 100vh;
+`
+
+const StyledMap = styled(Map)`
+  z-index: 1;
+  height: 100%;
+`
+
+const StyledMapButton = styled.button`
+  position: absolute;
+  bottom: 5%;
+  z-index: 2;
+  height: 50px;
+  width: 50px;
+`
+
+export { StyledMapDiv, StyledMap, StyledMapButton }
