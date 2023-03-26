8ea63a442701e1a23e56884414ba16b55f39b8de
knh6269 <knh6269@gmail.com>
2023-03-26 20:58:49 +0900

    feat : add register detail pages (#3) 회원가입 페이지 추가

---
+++ pages/Register/auth/auth.js
@@ -0,0 +1,12 @@
+import Register from '../index'
+
+const Auth = () => {
+
+  return (
+     <Register>
+        s
+     </Register>
+  )
+}
+
+export default Auth
---
+++ pages/Register/college/college.js
@@ -0,0 +1,12 @@
+import Register from "../index";
+
+const College = () => {
+
+  return (
+     <Register>
+        
+     </Register>
+  )
+}
+
+export default College
