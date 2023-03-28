3731dfa22d15b47323cda270ed2d74a65898a916
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-29 04:40:40 +0900

    feat : update markerInfo/index.js (#9)

--- components/markerInfo/index.js
+++ components/markerInfo/index.js
@@ -1,7 +1,31 @@
-import {} from './style'
+import { MarkerInfoDiv, PName, PStatus, PAddress, PTime, PCall } from './style'
+import { GiPositionMarker } from 'react-icons/gi'
+import { AiOutlineClockCircle } from 'react-icons/ai'
+import { IoIosCall } from 'react-icons/io'
 
 const MarkerInformation = (props) => {
-  return <div></div>
+  return (
+    <MarkerInfoDiv>
+      <div>
+        <PName>{props.info['name']}</PName>
+        <PStatus status={props.info['status'] === 'OPEN' ? 'green' : 'red'}>
+          {props.info['status'] === 'OPEN' ? '영업중' : '영업 종료'}
+        </PStatus>
+      </div>
+      <div>
+        <GiPositionMarker style={{ display: 'inline-block' }} />
+        <PAddress>{props.info['address']}</PAddress>
+      </div>
+      <div>
+        <AiOutlineClockCircle style={{ display: 'inline-block' }} />
+        <PTime>{props.info['time']}</PTime>
+      </div>
+      <div>
+        <IoIosCall style={{ display: 'inline-block' }} />
+        <PCall>{props.info['call']}</PCall>
+      </div>
+    </MarkerInfoDiv>
+  )
 }
 
 export default MarkerInformation
