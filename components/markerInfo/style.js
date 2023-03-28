d0b043bd7907389a68845a51e6cc96907a80581a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-29 05:20:28 +0900

    feat : update markerInfo/style.js (#9) font-weight 속성 추가

--- components/markerInfo/style.js
+++ components/markerInfo/style.js
@@ -16,27 +16,32 @@ const MarkerInfoDiv = styled.div`
 const PName = styled.p`
   font-size: 20px;
   display: inline-block;
+  font-weight: 500;
 `
 
 const PStatus = styled.p`
   display: inline-block;
   font-size: 15px;
   color: ${(props) => props.status};
+  font-weight: 500;
 `
 
 const PAddress = styled.p`
   display: inline-block;
   font-size: 10px;
+  font-weight: 400;
 `
 
 const PTime = styled.p`
   display: inline-block;
   font-size: 10px;
+  font-weight: 400;
 `
 
 const PCall = styled.p`
   display: inline-block;
   font-size: 10px;
+  font-weight: 400;
 `
 
 export { MarkerInfoDiv, PName, PStatus, PAddress, PTime, PCall }
