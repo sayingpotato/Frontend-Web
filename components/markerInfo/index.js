d0d049789d030877f467f3b81708c05b32404ca9
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-29 12:41:32 +0900

    feat : update markerInfo/index.js (#9) 작은 정보창 눌렀을 때 세부 페이지로 이동 함수

--- components/markerInfo/index.js
+++ components/markerInfo/index.js
@@ -4,27 +4,32 @@ import { AiOutlineClockCircle } from 'react-icons/ai'
 import { IoIosCall } from 'react-icons/io'
 
 const MarkerInformation = (props) => {
+  const handleMoreMarkerInfo = () => {
+    console.log('페이지 이동')
+  }
   return (
-    <MarkerInfoDiv>
-      <div>
-        <PName>{props.info['name']}</PName>
-        <PStatus status={props.info['status'] === 'OPEN' ? 'green' : 'red'}>
-          {props.info['status'] === 'OPEN' ? '영업중' : '영업 종료'}
-        </PStatus>
-      </div>
-      <div>
-        <GiPositionMarker style={{ display: 'inline-block' }} />
-        <PAddress>{props.info['address']}</PAddress>
-      </div>
-      <div>
-        <AiOutlineClockCircle style={{ display: 'inline-block' }} />
-        <PTime>{props.info['time']}</PTime>
-      </div>
-      <div>
-        <IoIosCall style={{ display: 'inline-block' }} />
-        <PCall>{props.info['call']}</PCall>
-      </div>
-    </MarkerInfoDiv>
+    <div onClick={handleMoreMarkerInfo}>
+      <MarkerInfoDiv>
+        <div>
+          <PName>{props.info['name']}</PName>
+          <PStatus status={props.info['status'] === 'OPEN' ? 'green' : 'red'}>
+            {props.info['status'] === 'OPEN' ? '영업중' : '영업 종료'}
+          </PStatus>
+        </div>
+        <div>
+          <GiPositionMarker style={{ display: 'inline-block' }} />
+          <PAddress>{props.info['address']}</PAddress>
+        </div>
+        <div>
+          <AiOutlineClockCircle style={{ display: 'inline-block' }} />
+          <PTime>{props.info['time']}</PTime>
+        </div>
+        <div>
+          <IoIosCall style={{ display: 'inline-block' }} />
+          <PCall>{props.info['call']}</PCall>
+        </div>
+      </MarkerInfoDiv>
+    </div>
   )
 }
 
