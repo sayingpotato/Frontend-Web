e7d15bba51696e870f1e80f9797bc74af05cb486
knh6269 <knh6269@gmail.com>
2023-03-31 22:10:40 +0900

    refactor : update line component (#3) 라인 컴포넌트 수정

--- components/line/style.js
+++ components/line/style.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 const LineComponent = styled.hr`
-    width : 90%;
-    border: 0.2px solid #B0B0B0;
+    border: 0.01px solid #B0B0B0;
     opacity: 0.7 ;
+    margin: 10px 20px;
 `;
 
 export {LineComponent};
