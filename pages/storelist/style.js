7bc135de46dfdf8e02e2b351e97ade4d750bf746
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-03 02:09:29 +0900

    feat : update storelist/style.js (#11)

--- pages/storelist/style.js
+++ pages/storelist/style.js
@@ -10,6 +10,10 @@ const DataDiv = styled.div`
   border: 1px solid black;
   border-radius: 1px 1px 5px 5px;
   width: 90%;
+  box-shadow: -5px 5px 5px -5px;
+  margin: 0 auto;
+  height: 50%;
+  margin-bottom: 5%;
 `
 
 export { Title, DataDiv }
