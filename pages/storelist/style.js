03d66bb52e72b30faa76423c3e2b18247fc68abb
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-03 04:01:57 +0900

    feat : update storelist/style.js (#11) 가게명 최대 할인 최소 할인 스타일

--- pages/storelist/style.js
+++ pages/storelist/style.js
@@ -16,4 +16,42 @@ const DataDiv = styled.div`
   margin-bottom: 5%;
 `
 
-export { Title, DataDiv }
+const TitleDiv = styled.div`
+  display: flex;
+  align-items: center;
+  height: 10%;
+`
+
+const StoreName = styled.p`
+  font-size: 15px;
+  font-weight: 700;
+  margin-left: 5%;
+  margin-right: 5%;
+`
+
+const StoreMin = styled.p`
+  font-size: 8px;
+  margin-right: 2%;
+  font-weight: 900;
+  color: white;
+  padding: 1% 2.5% 1% 2.5%;
+  border: 1px solid #4aa406;
+  border-radius: 1px;
+  background-color: #4aa406;
+`
+
+const StoreMax = styled.p`
+  font-size: 8px;
+  font-weight: 900;
+  color: white;
+  padding: 1% 2.5% 1% 2.5%;
+  border: 1px solid #ff2e2e;
+  border-radius: 1px;
+  background-color: #ff2e2e;
+`
+
+const ContentDiv = styled.div`
+  display: flex;
+`
+
+export { Title, DataDiv, TitleDiv, StoreName, StoreMin, StoreMax, ContentDiv }
