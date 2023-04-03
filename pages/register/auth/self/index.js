84e05b91ce6cfeccaa28a020c3cefca02c141515
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-03 23:31:03 +0900

    fix : update register (#16) 이미지 파일명 변경으로 인하여 수정

--- pages/register/auth/self/index.js
+++ pages/register/auth/self/index.js
@@ -1,62 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 import Register from '../../index'
 import { InfoComponent } from '@pages/register/info/id/style'
 import { Authdiv } from './style'
 import AuthButton from '@components/authButton/index'
-import right from '@public/images/Check circle.svg'
+import right from '@public/images/checkCircle.svg'
 import Image from 'next/image'
-import Next from "@components/next";
-import { RegisterLevel } from "@utils/atom.js"
-import { useSetRecoilState} from "recoil"
-import Label from "@components/label";
+import Next from '@components/next'
+import { RegisterLevel } from '@utils/atom.js'
+import { useSetRecoilState } from 'recoil'
+import Label from '@components/label'
 
 const Auth = () => {
-   const [state, setState] = useState(0);
-   const setRegisterLevel = useSetRecoilState(RegisterLevel);
+  const [state, setState] = useState(0)
+  const setRegisterLevel = useSetRecoilState(RegisterLevel)
 
-   const onClickAuthButton = () => {
-      if (state === 2) {
-         setState(1);
-         return ;
-      } 
-      setState(2)
-   }
+  const onClickAuthButton = () => {
+    if (state === 2) {
+      setState(1)
+      return
+    }
+    setState(2)
+  }
 
-   useEffect(() => {
-      setRegisterLevel(0);
-  },[]);
+  useEffect(() => {
+    setRegisterLevel(0)
+  }, [])
 
   return (
-     <Register>
+    <Register>
       <InfoComponent>
-         <Label
-            title={'휴대폰 인증'}/> 
-         <Authdiv>
-            <AuthButton 
-               onClick = {onClickAuthButton}
-               title = "인증하기"/>
-               {state === 2 &&
-                  <Image 
-                     src={right}
-                     alt="right"
-                     className="right"/>}
-         </Authdiv>
+        <Label title={'휴대폰 인증'} />
+        <Authdiv>
+          <AuthButton onClick={onClickAuthButton} title="인증하기" />
+          {state === 2 && <Image src={right} alt="right" className="right" />}
+        </Authdiv>
       </InfoComponent>
-      <Next 
-         state = {state}
-         nextView={'agree'}/>
-     </Register>
+      <Next state={state} nextView={'agree'} />
+    </Register>
   )
 }
 
 Auth.Layout = ({ children }) => {
-   return (
-      <div>
-       {children}
-      </div>
-   );
- };
- 
-Auth.Layout.displayName = 'RegisterLayout';
+  return <div>{children}</div>
+}
+
+Auth.Layout.displayName = 'RegisterLayout'
 
 export default Auth
