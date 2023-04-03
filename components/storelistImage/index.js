27a21635feb016bd384ab5996add3d3a95d71718
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-03 16:54:54 +0900

    feat : update storelistImage/index.js (#11) 가게 이미지 수정

--- components/storelistImage/index.js
+++ components/storelistImage/index.js
@@ -15,64 +15,35 @@ import {
 const StorelistImage = (props) => {
   const imageSrc = JSON.parse(props.imageSrc)
 
-  const firstParentRef = useRef()
-  const secondParentRef = useRef()
-  const thirdParentRef = useRef()
-  const fourthParentRef = useRef()
-
-  const [firstParentImage, setFirstParentImage] = useState(0)
-  const [secondParentImage, setSecondParentImage] = useState(0)
-  const [thirdParentImage, setThirdParentImage] = useState(0)
-  const [fourthParentImage, setFourthParentImage] = useState(0)
-
-  useEffect(() => {
-    const firstParentWidth = firstParentRef.current.offsetWidth
-    setFirstParentImage(firstParentWidth)
-    const secondParentWidth = secondParentRef.current.offsetWidth
-    setSecondParentImage(secondParentWidth)
-    const thirdParentWidth = thirdParentRef.current.offsetWidth
-    setThirdParentImage(thirdParentWidth)
-    const fourthParentWidth = fourthParentRef.current.offsetWidth
-    setFourthParentImage(fourthParentWidth)
-  }, [firstParentRef, secondParentRef, thirdParentRef, fourthParentRef])
-
   return (
     <ImageWholeDiv>
       <One>
-        <FirstImageDiv ref={firstParentRef}>
-          <Image
+        <FirstImageDiv>
+          <img
             src={`${imageSrc['firstImage']}`}
-            alt="첫 번째 이미지"
-            width={firstParentImage}
-            height={100}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
         </FirstImageDiv>
       </One>
       <Two>
-        <SecondImageDiv ref={secondParentRef}>
-          <Image
+        <SecondImageDiv>
+          <img
             src={`${imageSrc['secondImage']}`}
-            alt="두 번째 이미지"
-            width={secondParentImage}
-            height={50}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
         </SecondImageDiv>
-        <ThirdImageDiv ref={thirdParentRef}>
-          <Image
+        <ThirdImageDiv>
+          <img
             src={`${imageSrc['thirdImage']}`}
-            alt="세 번째 이미지"
-            width={thirdParentImage}
-            height={50}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
         </ThirdImageDiv>
       </Two>
       <Three>
-        <FourthImageDiv ref={fourthParentRef}>
-          <Image
+        <FourthImageDiv>
+          <img
             src={`${imageSrc['fourthImage']}`}
-            alt="네 번째 이미지"
-            width={fourthParentImage}
-            height={100}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
         </FourthImageDiv>
       </Three>
