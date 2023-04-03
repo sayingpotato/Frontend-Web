69c5d2e81c32475525328a455982b8f3d7fde4fe
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-03 16:55:09 +0900

    feat : update storelistImage/style.js (#11) 가게 이미지 수정

--- components/storelistImage/style.js
+++ components/storelistImage/style.js
@@ -3,42 +3,45 @@ import styled from 'styled-components'
 const ImageWholeDiv = styled.div`
   display: flex;
   justify-content: space-between;
-  height: 100px;
-`
-
-const MiddleDiv = styled.div`
-  flex-direction: column;
+  height: 20%;
+  padding: 0;
 `
 
 const FirstImageDiv = styled.div`
   width: 100%;
+  height: 100%;
 `
 
 const SecondImageDiv = styled.div`
   width: 100%;
+  height: 50%;
 `
 
 const ThirdImageDiv = styled.div`
   width: 100%;
+  height: 50%;
 `
 
 const FourthImageDiv = styled.div`
   width: 100%;
+  height: 100%;
 `
 
 const One = styled.div`
   width: 50%;
+  height: 100;
 `
 const Two = styled.div`
   width: 25%;
+  height: 100;
 `
 const Three = styled.div`
   width: 25%;
+  height: 100;
 `
 
 export {
   ImageWholeDiv,
-  MiddleDiv,
   FirstImageDiv,
   SecondImageDiv,
   ThirdImageDiv,
