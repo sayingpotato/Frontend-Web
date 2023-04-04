d973c91e07412c03b157eed838312eee5dfe6fe2
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-04 22:07:30 +0900

    feat : update todaydiscount/style.js (#19)

--- pages/todaydiscount/style.js
+++ pages/todaydiscount/style.js
@@ -1,37 +1,78 @@
 import styled from 'styled-components'
 
 const InfoDiv = styled.div`
-  background-color: blue;
-  height: 13%;
+  background-color: #28469c;
+  height: 15%;
   padding-top: 2%;
 `
 
 const TypeButtonDiv = styled.div`
   display: flex;
   justify-content: center;
+  margin-top: 1%;
 `
 
-const TypeButton = styled.button`
-  width: 55px;
+const TypeCafeButton = styled.button`
+  width: 50px;
   font-size: 10px;
+  height: 20px;
+  background-color: ${(props) => (props.state === 'cafe' ? 'black' : 'white')};
+  color: ${(props) => (props.state === 'cafe' ? 'white' : 'black')};
+  border: 0px solid ${(props) => (props.state === 'cafe' ? 'black' : 'white')};
+  border-radius: 3px;
+`
+
+const TypeFoodButton = styled.button`
+  width: 50px;
+  font-size: 10px;
+  height: 20px;
+  background-color: ${(props) => (props.state === 'food' ? 'black' : 'white')};
+  color: ${(props) => (props.state === 'food' ? 'white' : 'black')};
+  border: 0px solid ${(props) => (props.state === 'cafe' ? 'black' : 'white')};
+  border-radius: 3px;
 `
 
 const DayInfoDiv = styled.div`
   display: flex;
   justify-content: space-around;
+  align-items: center;
+`
+
+const NotNowDay = styled.p`
+  color: white;
+  font-weight: 400;
+  font-size: 20px;
+  margin-top: 10px;
+`
+
+const NowDay = styled.p`
+  color: white;
+  font-weight: 800;
+  font-size: 32px;
+  margin-top: 10px;
 `
 
 const BackDiv = styled.div`
-  background-color: grey;
+  background-color: #efefef;
   padding: 3%;
-  height: 70%;
+  height: 100%;
 `
 
 const WholeDiv = styled.div`
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
-  gap: 5px;
+  gap: 15px;
+  box-sizing: border-box;
 `
 
-export { InfoDiv, TypeButtonDiv, TypeButton, DayInfoDiv, BackDiv, WholeDiv }
+export {
+  InfoDiv,
+  TypeButtonDiv,
+  TypeFoodButton,
+  TypeCafeButton,
+  DayInfoDiv,
+  NotNowDay,
+  NowDay,
+  BackDiv,
+  WholeDiv,
+}
