c7b925adf74c7f1f049e740b0dda9a8055d26340
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-04 15:49:38 +0900

    feat : update discountTable/style.js (#19)

---
+++ components/discountTable/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Div = styled.div`
+  border-radius: 5px;
+  width: 70px;
+  height: 70px;
+  border: 0px solid white;
+  padding: 3%;
+  background-color: white;
+`
+
+export { Div }
