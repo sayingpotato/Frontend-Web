eec3765380d630d30c9f18ce386b18c162cb0d57
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-04 15:47:09 +0900

    feat : udpate date.js (#19) 요일명 한글로 수정

--- utils/date.js
+++ utils/date.js
@@ -1,12 +1,12 @@
 const date = () => {
   const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
   ]
   const today = new Date()
   const dayName = days[today.getDay()]
