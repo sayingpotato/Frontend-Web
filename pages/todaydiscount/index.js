479160a160202c594903914391424a093e23d689
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-04 15:46:22 +0900

    feat : udpate todaydiscount/index.js (#19) 오늘의 요일을 통한 어제와 내일 요일 구하기

--- pages/todaydiscount/index.js
+++ pages/todaydiscount/index.js
@@ -3,8 +3,37 @@ import { useEffect, useState, useRef } from 'react'
 import date from '@utils/date'
 
 const TodayDiscount = () => {
+  const [nowDay, setNowDay] = useState('')
+  const [prevDay, setPrevDay] = useState('')
+  const [nextDay, setNextDay] = useState('')
+
+  const days = [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ]
+
   useEffect(() => {
-    console.log(date())
+    setNowDay(date())
+
+    const idx = days.indexOf(nowDay)
+
+    if (idx === 0) {
+      setPrevDay('토요일')
+      setNextDay(days[idx + 1])
+      return
+    } else if (idx === 6) {
+      setNextDay('일요일')
+      setPrevDay(days[idx - 1])
+      return
+    } else {
+      setNextDay(days[idx + 1])
+      setPrevDay(days[idx - 1])
+    }
   }, [])
   return <div></div>
 }
