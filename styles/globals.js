adad5846170265afb743b38632aee25e7412dc63
knh6269 <knh6269@gmail.com>
2023-04-07 16:41:23 +0900

    add : add globals(#14) 글로벌 width, height 추가

--- styles/globals.js
+++ styles/globals.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: "Pretendard Variable";
+    width: 100vw;
+    height: 100vh;
     margin: 0;
   }
 `;
