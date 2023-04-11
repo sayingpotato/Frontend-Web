4db9f6098b2d94e2caacf52883e16c746f9af9c3
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-11 22:19:49 +0900

    feat : update orderedContent/index.js (#21) 메뉴 개수에 따른 변화

--- components/orderedContent/index.js
+++ components/orderedContent/index.js
@@ -1,10 +1,58 @@
-  const [reviewTitle, setReviewTitle] = useState('리뷰하기')
+import { useEffect, useState, useRef } from 'react'
+
+import {
+  Div,
+  ContentDiv,
+  StoreDiv,
+  PName,
+  MenuDiv,
+  PMenu,
+  MoreButton,
+  StyledImage,
+  ReviewDiv,
+  ReviewButton,
+  ReviewContent,
+} from './style'
+
+import Review from '@components/reviewContent'
+
+const OrderedContent = (props) => {
+  const value = JSON.parse(props.data)
+
   const [open, setOpen] = useState(false)
 
   const reviewButtonClick = () => {
-    setReviewTitle('리뷰가 열렸다네')
     setOpen(!open)
   }
+
+  let data = value['menu']
+
+  const menuData = () => {
+    const result = []
+    if (data.length <= 5) {
+      for (let i = 0; i < data.length; i++) {
+        result.push(<PMenu key={i}>{data[i]}</PMenu>)
+      }
+    } else {
+      for (let i = 0; i < 5; i++) {
+        result.push(<PMenu key={i}>{data[i]}</PMenu>)
+      }
+      result.push(<PMenu key={5}>...</PMenu>)
+    }
+
+    return result
+  }
+
+  return (
+    <Div state={open}>
+      <ContentDiv>
+        <StyledImage src={value['imageSrc']} alt="logo" />
+        <StoreDiv>
+          <PName>{value['name']}</PName>
+          <MenuDiv>{menuData()}</MenuDiv>
+          <MoreButton>주문 상세</MoreButton>
+        </StoreDiv>
+      </ContentDiv>
       <ReviewDiv state={open}>
         <ReviewButton onClick={reviewButtonClick}>
           {open ? '▲ 접기' : '▼ 리뷰하기'}
@@ -13,3 +61,8 @@
           <Review />
         </ReviewContent>
       </ReviewDiv>
+    </Div>
+  )
+}
+
+export default OrderedContent
