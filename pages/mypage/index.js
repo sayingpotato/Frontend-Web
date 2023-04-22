bc0c0b46ec590f54117570884eefd6ee722004ce
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-23 00:19:21 +0900

    feat : update mypage/index.js (#26)

--- pages/mypage/index.js
+++ pages/mypage/index.js
@@ -1,3 +1,18 @@
+import { useEffect, useState, useRef } from 'react'
+import Image from 'next/image';
+import Link from 'next/link'
+
+import Title from '@components/title'
+
+import MoneySrc from '@public/images/coin.svg'
+import PotatoSrc from '@public/images/modify.svg'
+import NoticeSrc from '@public/images/notice.svg'
+import MedalSrc from '@public/images/medal.svg'
+import ChartSrc from '@public/images/chart.svg'
+import CallSrc from '@public/images/call.svg'
+
+import { UserInfo, UserName, DiscountDiv, MoneyTitle, MoneyDiv, Money, BackDiv, DiscountInfo, DiscountTitle, ButtonWholeDiv, ButtonDiv, ButtonTitle } from './style'
+
 const ImageLists = [
     { image: PotatoSrc, title: '프로필 수정', next: 'modify' },
     { image: NoticeSrc, title: '공지사항', next: 'notice' },
@@ -5,3 +20,42 @@ const ImageLists = [
     { image: ChartSrc, title: '통계 보기', next: 'mychart' },
     { image: CallSrc, title: '고객센터', next: 'service' }
   ]
+
+const mypage = () => {
+
+  return (
+    <>
+      <Title>마이페이지</Title>
+      <UserInfo>
+        <UserName>권낙현님</UserName>
+        <DiscountDiv>
+            <MoneyTitle>할인금액</MoneyTitle>
+            <MoneyDiv>
+                <Image src={MoneySrc} alt="logo" />
+                <Money>15000</Money>
+            </MoneyDiv>
+        </DiscountDiv>
+      </UserInfo>
+      <BackDiv>
+        <DiscountInfo>
+            <DiscountTitle>최근 받은 할인</DiscountTitle>
+        </DiscountInfo>
+        {ImageLists.map((item, index) => {
+                const nextUrl = `/${item.next}`
+                return (
+                    <ButtonWholeDiv key={index}>
+                        <Link href={nextUrl}>
+                        <ButtonDiv>
+                            <Image src={item.image} alt={item.title} />
+                            <ButtonTitle>{item.title}</ButtonTitle>
+                        </ButtonDiv>
+                        </Link>
+                    </ButtonWholeDiv>
+                )
+            })}
+      </BackDiv>
+    </>
+  )
+}
+
+export default mypage
