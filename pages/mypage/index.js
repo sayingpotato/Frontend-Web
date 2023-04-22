d676f1af773dd3fa10b767cc8f61d502bf23a12a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-23 00:18:48 +0900

    feat : update mypage/index.js (#26) 마이페이지 각 버튼 list화

---
+++ pages/mypage/index.js
@@ -0,0 +1,7 @@
+const ImageLists = [
+    { image: PotatoSrc, title: '프로필 수정', next: 'modify' },
+    { image: NoticeSrc, title: '공지사항', next: 'notice' },
+    { image: MedalSrc, title: '내 랭킹 보기', next: 'myrank' },
+    { image: ChartSrc, title: '통계 보기', next: 'mychart' },
+    { image: CallSrc, title: '고객센터', next: 'service' }
+  ]
