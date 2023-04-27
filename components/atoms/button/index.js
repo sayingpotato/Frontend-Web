046d61d4aca8d6b45310eb4afe37d3d3aa5604b5
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-27 23:45:21 +0900

    feat : update components/atoms/button/index.js (#32)

--- components/atoms/button/index.js
+++ components/atoms/button/index.js
@@ -1,6 +1,6 @@
 import { StyledButton } from './style';
 
-const Button = ({ text, onClick, className}) => {
+const Button = ({ text, onClick, className }) => {
 
     return (
         <StyledButton onClick={onClick} className={className}>{text}</StyledButton>
