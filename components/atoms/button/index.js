44712e8d4b4de1b5301f970567fe5b8a769cceeb
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-27 23:34:35 +0900

    feat : add components/atoms/button/index.js (#32)

---
+++ components/atoms/button/index.js
@@ -0,0 +1,10 @@
+import { StyledButton } from './style';
+
+const Button = ({ text, onClick, className}) => {
+
+    return (
+        <StyledButton onClick={onClick} className={className}>{text}</StyledButton>
+    );
+};
+
+export default Button;
