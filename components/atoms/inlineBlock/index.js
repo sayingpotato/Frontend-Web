7f64f33b875fa3e8332148f3490ef796e0db1368
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-27 23:36:39 +0900

    feat : add components/atoms/inlineBlock/index.js (#32)

---
+++ components/atoms/inlineBlock/index.js
@@ -0,0 +1,10 @@
+import { StyledInlineBlock } from './style';
+
+const InlineBlock = ({ className}) => {
+
+    return (
+        <StyledInlineBlock className={className} />
+    );
+};
+
+export default InlineBlock;
