5862af4858bc97e121d1727c7a32d0cd76ad39b4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-04 01:31:39 +0900

    feat : add molecules/title (#32)

---
+++ components/molecules/title/index.js
@@ -0,0 +1,11 @@
+import Text from "@atoms/text"
+
+const Title = ({text}) => {
+    return (
+        <div>
+            <Text text={text} className="title"/>
+        </div>
+    );
+};
+
+export default Title;
