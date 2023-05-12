0320234e4a9dbf3ecf9383acd4aac6fc3bca298d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-12 19:00:33 +0900

    feat : update organisms/store/index.js (#32)

--- components/organisms/store/index.js
+++ components/organisms/store/index.js
@@ -1,26 +1,22 @@
-import { StyledStoreInfoDiv, StyledReviewInfoDiv, StyledImageTextWholeDiv } from './style';
+import { StyledStoreInfoDiv, StyledReviewInfoDiv } from './style';
 
 import Text from "@atoms/text"
 import ImageLayout from "@molecules/imageLayout"
 import InlineText from "@molecules/inlineText"
-import PercentBlock from "@molecules/percentBlock"
-import ImageText from "@molecules/imageText"
+import MenuRanking from '@organisms/menuRanking';
+import Review from '@organisms/review';
 
-const Store = () => {
+const Store = ({storeName, imageSrc, minDiscount, maxDisount}) => {
     return (
         <div>
             <ImageLayout />
             <StyledStoreInfoDiv>
-                <Text text="가게 이름" className="storeName" />
-                <InlineText text1="최소 할인율" text1ClassName="minDiscount" text2="최대 할인율" text2ClassName="maxDiscount"/>
+                <Text text={storeName} className="storeName" />
+                <InlineText text1={minDiscount} text1ClassName="minDiscount" text2={maxDisount} text2ClassName="maxDiscount"/>
             </StyledStoreInfoDiv>
             <StyledReviewInfoDiv>
-                <StyledImageTextWholeDiv>
-                    <ImageText src="images/goldMedal.svg" alt="images/goldMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
-                    <ImageText src="images/silverMedal.svg" alt="images/silverMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
-                    <ImageText src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
-                </StyledImageTextWholeDiv>
-                <PercentBlock />
+                <MenuRanking />
+                <Review />
             </StyledReviewInfoDiv>
         </div>
     );
