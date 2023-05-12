9c14e07b03b81e324a2636d2fd56febe0c47a39a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-12 18:59:12 +0900

    feat : add molecules/inlineText/style.js (#32)

---
+++ components/molecules/inlineText/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledInlineTextDiv = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  gap: 5px;
+  padding: 0;
+`
+
+export { StyledInlineTextDiv };
