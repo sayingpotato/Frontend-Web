a55bb782803a3b561b507f6067265bf5d1e9adaa
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-12 19:01:10 +0900

    feat : update templates/storelistTempalte/index.js (#32)

--- components/templates/storelistTemplate/index.js
+++ components/templates/storelistTemplate/index.js
@@ -1,14 +1,31 @@
-import Title from "@molecules/title"
+import Router, { useRouter } from 'next/router'
+import { StorelistTemplateDiv } from './style';
+
 import ImageButton from "@molecules/imageButton"
 import Store from "@organisms/store"
 
-const StorelistTemplate = () => {
+const StorelistTemplate = ({storeName, imageSrc, minDiscount, maxDisount}) => {
+    const router = useRouter()
+    const routerValue = [router['query']['lat'], router['query']['lng']]
+
+    // map에서 중심 값을 통하여 정렬 받을 예정
+    console.log(routerValue)
+
+    const goMapButtonClick = () => {
+        router.push({
+        pathname: '/map',
+            query: { goBackLat: routerValue[0], goBackLng: routerValue[1] },
+        })
+    }
+
     return (
-        <div>
-            <Title text="내 주변 가게"/>
-            <Store />
-            <ImageButton className="mapImageButton" src="images/whiteMap.svg" alt="images/whiteMap.svg"/>
-        </div>
+        <StorelistTemplateDiv>
+            <Store storeName={storeName} 
+                imageSrc={imageSrc} 
+                minDiscount={minDiscount} 
+                maxDisount={maxDisount} />
+            <ImageButton className="mapImageButton" src="images/whiteMap.svg" alt="images/whiteMap.svg" onClick={goMapButtonClick}/>
+        </StorelistTemplateDiv>
     );
 };
 
