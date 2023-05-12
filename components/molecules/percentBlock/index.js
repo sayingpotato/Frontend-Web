f8c9cc1bc99ffb8e90c5975e5eceb5d919af13d4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-12 18:59:28 +0900

    feat : add molecules/percentBlock/index.js (#32)

---
+++ components/molecules/percentBlock/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { StyledPercentBlockDiv } from './style';
+import InlineBlock from "@atoms/inlineBlock"
+import ImageText from "@molecules/imageText"
+
+const PercentBlock = ({ children }) => {
+  return (
+    <StyledPercentBlockDiv>
+      {React.Children.map(children, child => (
+        <InlineBlock className="outside">
+          <InlineBlock className={`${child.props.inlineBlockClass}`}>
+            <ImageText src={child.props.src} alt={child.props.alt} text={child.props.text} />
+          </InlineBlock>
+        </InlineBlock>
+      ))}
+    </StyledPercentBlockDiv>
+  );
+};
+
+export default PercentBlock;
