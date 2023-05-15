177900897987c2bab7d3e1ded32bb54996ce1eb6
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-16 00:38:36 +0900

    feat : add templates/todayDiscountTemplate/index.js (#34)

---
+++ components/templates/todayDiscountTemplate/index.js
@@ -0,0 +1,14 @@
+import Router, { useRouter } from 'next/router'
+import { TodayDiscountTemplateDiv } from './style';
+
+const TodayDiscountTemplate = ({}) => {
+
+
+    return (
+        <TodayDiscountTemplateDiv>
+            
+        </TodayDiscountTemplateDiv>
+    );
+};
+
+export default TodayDiscountTemplate;
