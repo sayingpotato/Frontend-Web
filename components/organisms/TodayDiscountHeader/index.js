9e39e653ec708dd06a4767847461e5543befb93e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-16 23:28:26 +0900

    feat : update todayDiscountHeader/index.js (#34) 식당, 카페 div className 추가 변동

--- components/organisms/TodayDiscountHeader/index.js
+++ components/organisms/TodayDiscountHeader/index.js
@@ -4,4 +4,9 @@
                     text1OnClick={text1OnClick} text2OnClick={text2OnClick}
                     state={state}
                 />
-            </TypeButtonDiv>
+            </TypeButtonDiv>            <InlineButton text1="식당" text2="카페" 
+                text1ClassName="typeCafeButton" text2ClassName="typeFoodButton"
+                text1OnClick={text1OnClick} text2OnClick={text2OnClick}
+                state={state}
+                divClassName="center"
+            />
