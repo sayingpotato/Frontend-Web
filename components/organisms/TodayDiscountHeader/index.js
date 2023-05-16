8667b1d887c9d9fea09d4c7e9dad10d41974e3e1
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-16 23:21:58 +0900

    feat : udpate organisms/todayDiscountHeader/index.js (#34) molecules/InlineButton를 이용한 식당, 카페 버튼

---
+++ components/organisms/TodayDiscountHeader/index.js
@@ -0,0 +1,7 @@
+            <TypeButtonDiv>
+                <InlineButton text1="식당" text2="카페" 
+                    text1ClassName="typeCafeButton" text2ClassName="typeFoodButton"
+                    text1OnClick={text1OnClick} text2OnClick={text2OnClick}
+                    state={state}
+                />
+            </TypeButtonDiv>
