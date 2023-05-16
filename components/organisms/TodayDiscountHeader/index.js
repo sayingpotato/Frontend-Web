3e505087832866eedeb9b8d5e839b1ddb7dffc78
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-16 23:57:58 +0900

    feat : udpate organisms/todayDiscountHeader/index.js (#34)

--- components/organisms/TodayDiscountHeader/index.js
+++ components/organisms/TodayDiscountHeader/index.js
@@ -1,12 +1,22 @@
-            <TypeButtonDiv>
-                <InlineButton text1="식당" text2="카페" 
-                    text1ClassName="typeCafeButton" text2ClassName="typeFoodButton"
-                    text1OnClick={text1OnClick} text2OnClick={text2OnClick}
-                    state={state}
-                />
-            </TypeButtonDiv>            <InlineButton text1="식당" text2="카페" 
+import { TodayDiscountHeaderDiv } from './style';
+
+import InlineButton from "@molecules/inlineButton"
+import DayInfo from "@organisms/dayInfo"
+
+const TodayDiscountHeader = ({FoodOnClick, CafeOnClick, state, prevDay, nowDay, nextDay}) => {
+    return (
+        <TodayDiscountHeaderDiv>
+            <InlineButton text1="식당" text2="카페" 
                 text1ClassName="typeCafeButton" text2ClassName="typeFoodButton"
-                text1OnClick={text1OnClick} text2OnClick={text2OnClick}
+                text1OnClick={FoodOnClick} text2OnClick={CafeOnClick}
                 state={state}
                 divClassName="center"
             />
+            <DayInfo mainClassName='mainTitle' subClassName='subTitle' 
+                prevDay={prevDay} nowDay={nowDay} nextDay={nextDay}
+            />
+        </TodayDiscountHeaderDiv>
+    );
+};
+
+export default TodayDiscountHeader;
