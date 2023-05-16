dd92fc13542e32b75716e6f84dcdac45ba4d76da
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-17 01:10:30 +0900

    feat : update organisms/todayDiscountContent/style.js (#34)

---
+++ components/organisms/TodayDiscountContent/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TodayDiscountContentDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  justify-content: center;
+`
+
+export { TodayDiscountContentDiv };
