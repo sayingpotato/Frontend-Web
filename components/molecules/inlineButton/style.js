e6c8188d2b1a74bd950abe107b2937c6fd079c0e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-16 23:55:44 +0900

    feat : udpate molecules/inlineButton/style.js (#34) div에도 props className 추가

--- components/molecules/inlineButton/style.js
+++ components/molecules/inlineButton/style.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 const StyledInlineButtontDiv = styled.div`
   display: flex;
   align-items: center;
-  height: 50px;
+  height: 30px;
   gap: 5px;
   padding: 0;
+
+  &.center {
+    justify-content: center;
+  }
 `
 
 export { StyledInlineButtontDiv };
