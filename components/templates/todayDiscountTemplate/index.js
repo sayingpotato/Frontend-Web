3ed785a314b1a66c8f3fe4bfc37769d64c0610e1
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-17 01:10:57 +0900

    feat : update templates/todayDiscountTemplate/index.js (#34)

--- components/templates/todayDiscountTemplate/index.js
+++ components/templates/todayDiscountTemplate/index.js
@@ -1,12 +1,14 @@
 import Router, { useRouter } from 'next/router'
+
 import { TodayDiscountTemplateDiv } from './style';
+import TodayDiscountContent from '@organisms/todayDiscountContent';
 
-const TodayDiscountTemplate = ({}) => {
-
+const TodayDiscountTemplate = ({data}) => {
+    const value = JSON.parse(data)
 
     return (
         <TodayDiscountTemplateDiv>
-            
+            <TodayDiscountContent src={value['imageSrc']} alt="logo" text={value['name']} />
         </TodayDiscountTemplateDiv>
     );
 };
