02fb33376a190a3e8ca379f1ff8be3627e9ede64
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-18 00:06:43 +0900

    feat : update organisms/TodayDiscountContent/style.js (#34)

--- components/organisms/TodayDiscountHeader/style.js
+++ components/organisms/TodayDiscountHeader/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const TodayDiscountHeaderDiv = styled.div`
   background-color: #28469c;
-  height: 15%;
+  height: 100px;
 `
 
 export { TodayDiscountHeaderDiv };
