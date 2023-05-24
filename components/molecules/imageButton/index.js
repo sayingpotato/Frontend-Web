f1c01adb56cc12a68aa18ac82a60d843ca96c08f
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-25 00:32:46 +0900

    feat : update molecules/imageButton/index.js (#36) state 상태 값 추가

--- components/molecules/imageButton/index.js
+++ components/molecules/imageButton/index.js
@@ -1,10 +1,10 @@
 import Button from "@atoms/button";
 import Image from "@atoms/image";
 
-const ImageButton = ({ className, src, alt, onClick }) => {
+const ImageButton = ({ className, src, alt, onClick, state }) => {
     return (
         <>
-            <Button className={className} onClick={onClick}>
+            <Button className={className} onClick={onClick} state={state}>
                 <Image src={src} alt={alt} />
             </Button>
         </>
