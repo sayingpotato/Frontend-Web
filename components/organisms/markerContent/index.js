99fd2e3db28a698e6c9deb340168be49788fb567
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-25 00:38:32 +0900

    feat : add organisms/markerContent/index.js (#36) 하단 마커 정보창 모음집

---
+++ components/organisms/markerContent/index.js
@@ -0,0 +1,21 @@
+import Router, { useRouter } from 'next/router';
+import { MarkerContentDiv } from './style';
+
+import MarkerContentHeader from "@organisms/markerContentHeader"
+import MarkerContentInfo from "@organisms/markerContentInfo"
+
+const MarkerContent = ({state, info}) => {
+
+    const handleMoreMarkerInfo = () => {
+        console.log('페이지 이동')
+    }
+
+    return (
+        <MarkerContentDiv state={state} onClick={handleMoreMarkerInfo}>
+            <MarkerContentHeader name={info['name']} status={info['status'] === 'OPEN' ? 'green' : 'red'} statusText={info['status'] === 'OPEN' ? '영업중' : '영업 종료'}/>
+            <MarkerContentInfo address={info['address']} time={info['time']} call={info['call']} />
+        </MarkerContentDiv>
+    );
+};
+
+export default MarkerContent;
