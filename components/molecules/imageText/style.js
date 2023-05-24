47c07629f17ca3f9022e60e2c79c4bd4f6972c30
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-25 00:33:30 +0900

    feat : update molecules/imageText/style.js (#36)

--- components/molecules/imageText/style.js
+++ components/molecules/imageText/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledImageTextDiv = styled.div`
     display: flex;
     align-items: center;
-    height:30px;
+    gap : 3px;
 `
 
 export { StyledImageTextDiv };
