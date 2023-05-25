b13abf7eab926b86c358af82d10e1fdb3a161c66
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-25 23:49:01 +0900

    feat : update organisms/storeInformation/style.js (#37)

---
+++ components/organisms/storeInformation/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StoreInformationDiv = styled.div`
+  display: flex;
+  gap : 10px;
+  margin-left : 10px;
+  margin-right : 10px;
+`
+
+const TextInfoDiv = styled.div`
+    display : block;
+`
+
+export { StoreInformationDiv, TextInfoDiv };
