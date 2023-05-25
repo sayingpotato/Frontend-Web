2fc45be27be1c3c59cff3042d5e5da2c94660240
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-25 23:47:59 +0900

    feat : udpate molecules/imageText/style.js (#37)

--- components/molecules/imageText/style.js
+++ components/molecules/imageText/style.js
@@ -4,6 +4,10 @@ const StyledImageTextDiv = styled.div`
     display: flex;
     align-items: center;
     gap : 3px;
+
+    &.storeInformationPostion {
+        margin-left : 10px;
+    }
 `
 
 export { StyledImageTextDiv };
