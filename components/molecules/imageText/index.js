182dbcda0fdac211f8d1fcf8f54767e565918725
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-25 23:47:41 +0900

    feat : update molecules/imageText/index.js (#37)

--- components/molecules/imageText/index.js
+++ components/molecules/imageText/index.js
@@ -3,9 +3,9 @@ import { StyledImageTextDiv } from './style';
 import Text from "@atoms/text"
 import Image from "@atoms/image"
 
-const ImageText = ({src, alt, text, textClass}) => {
+const ImageText = ({src, alt, text, textClass, divClassName}) => {
     return (
-        <StyledImageTextDiv>
+        <StyledImageTextDiv className={divClassName}>
             <Image src={src} alt={alt}/>
             <Text text={text} className={textClass}/>
         </StyledImageTextDiv>
