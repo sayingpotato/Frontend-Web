0c471531d63086031e8e51c1987b2b510197cce4
knh6269 <knh6269@gmail.com>
2023-05-28 18:15:38 +0900

    feat: add fetcher (#41)

--- api/fetcher.js
+++ api/fetcher.js
@@ -1,5 +1,13 @@
-const fetcher = () => {
+import axios from "axios";
 
-}
+const fetcher = axios.create({
+    baseURL : "http://34.64.165.57:8080",
+    timeout: 2500
+})
+
+//권한이 필요한 api가 생길 시 추가 
+fetcher.interceptors.request.use((config) => {
+    return config;
+})
 
 export default fetcher;
