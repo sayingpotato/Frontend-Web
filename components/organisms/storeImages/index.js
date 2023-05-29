55d70f25a40c19dc7fe74de30fa9434cf01e657d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-29 16:40:22 +0900

    feat : add organisms/storeImages/index.js (#37) 가게 상세 페이지의 가게 내부 이미지

---
+++ components/organisms/storeImages/index.js
@@ -0,0 +1,26 @@
+import { StoreImagesDiv, ImagesDiv } from './style';
+
+import Image from "@atoms/image"
+import Text from "@atoms/text"
+
+const StoreImages = ({images}) => {
+
+    let imageData = images[0]
+
+    return (
+        <>
+            <StoreImagesDiv>
+                <Text text="가게 사진"/>
+                <ImagesDiv>
+                    <Image src={imageData['image1']} alt="images/storeImage.svg" />
+                    <Image src={imageData['image2']} alt="images/storeImage.svg" />
+                    <Image src={imageData['image3']} alt="images/storeImage.svg" />
+                    <Image src={imageData['image4']} alt="images/storeImage.svg" />
+                </ImagesDiv>    
+
+            </StoreImagesDiv>
+        </>
+    );
+};
+
+export default StoreImages;
