cdca44ee8f80f14243aa94f6aa0c4bae3885fc35
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-29 16:42:12 +0900

    feat : add templates/storedetailTemplate/index.js (#37)

---
+++ components/templates/storedetailTemplate/index.js
@@ -0,0 +1,33 @@
+import Router, { useRouter } from 'next/router'
+import { StoredetailTemplateDiv } from './style';
+
+import Line from "@atoms/line"
+import Title from "@molecules/title"
+import StoreInformation from "@organisms/storeInformation"
+import StoreVisitorReview from "@organisms/storeVisitorReview"
+import StoreImages from "@organisms/storeImages"
+import MenuImages from "@organisms/menuImages"
+
+const StorelistTemplate = ({data}) => {
+
+    let value = data[0]
+
+    const router = useRouter()
+    console.log(router)
+
+    return (
+        <StoredetailTemplateDiv>
+            <Title text={value['name']} />
+            <Line />
+            <StoreInformation address={value['address']} time={value['time']} call={value['call']} concent={value['concent']} rank={value['rank']} room={value['roomImage']}/>
+            <Line />
+            <StoreVisitorReview />
+            <Line />
+            <StoreImages images={value['storeImages']}/>
+            <Line />
+            <MenuImages images={value['menuImages']} name={value['menuNames']}/>
+        </StoredetailTemplateDiv>
+    );
+};
+
+export default StorelistTemplate;
