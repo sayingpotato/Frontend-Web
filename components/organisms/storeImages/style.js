14f43c0f8cae0fa1365d75da4bf14abfff5c634e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-29 16:40:31 +0900

    feat : add organisms/storeImages/style.js (#37) 가게 상세 페이지의 가게 내부 이미지

---
+++ components/organisms/storeImages/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StoreImagesDiv = styled.div`
+    margin-left : 10px;
+    margin-right : 10px;
+`
+
+const ImagesDiv = styled.div`
+    display : flex;
+    gap : 5px;
+`
+
+export { StoreImagesDiv, ImagesDiv };
