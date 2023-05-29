19fed173746693d354c687d2ffdbdb892e6718f3
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-05-29 16:40:02 +0900

    feat : add organisms/menuImages/style.js (#37) 가게 상세 페이지의 메뉴판

---
+++ components/organisms/menuImages/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const MenuImagesDiv = styled.div`
+    margin-left : 10px;
+    margin-right : 10px;
+`
+
+const MenuWholeImagesDiv = styled.div`
+    display: block;
+    text-align: center;
+`
+
+const ImagesDiv = styled.div`
+    display : flex;
+    gap : 20px;
+    justify-content: center;
+`
+
+export { MenuImagesDiv, MenuWholeImagesDiv, ImagesDiv };
