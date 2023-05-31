6ed1540be13558a6375a131be973278a29ff14ea
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 05:01:06 +0900

    feat : update organisms/mypageUserInfo/index.js (#46)

--- components/organisms/mypageUserInfo/index.js
+++ components/organisms/mypageUserInfo/index.js
@@ -1,13 +1,17 @@
 import { MypageUserInfoDiv } from './style';
 
-import InlineText from '@molecules/inlineText';
+import Text from '@atoms/text';
+import Title from '@molecules/title';
 import ImageText from '@molecules/imageText';
 
 const MypageUserInfo = () => {
     return (
         <MypageUserInfoDiv>
-            <InlineText textOne="권낙현님" textTwo="할인금액" textOneClassName="mypageUserName" textTwoClassName="mypageMoneyTitle" />
-            <ImageText text="15000" src="images/coin.svg" alt="images/coin.svg" textClassName="mypageMoneyTotal" />
+            <Title text="권낙현님" className="mypageName" />
+            <div>
+                <Text text="할인 금액" className="mypageMoneyTitle" />
+                <ImageText imageClassName="mypageMoneyIcon" divClassName="basic" text="15000" src="images/coin.svg" alt="images/coin.svg" textClassName="mypageMoneyTotal" />
+            </div>
         </MypageUserInfoDiv>
     );
 };
