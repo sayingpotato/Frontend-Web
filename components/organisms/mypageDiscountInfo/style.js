d317e18b24c411359283b3cd22f2a3e9ce186adf
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 04:35:56 +0900

    feat : add organisms/mypageDiscountInfo/style.js (#46)

---
+++ components/organisms/mypageDiscountInfo/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const MypageDiscountInfoDiv = styled.div`
+    background-color : #EFEFEF;
+    padding : 20px 30px 20px 30px;
+    height : 80vh;
+    gap : 10px;
+    display: flex;
+    flex-direction: column;
+`
+
+export { MypageDiscountInfoDiv };
