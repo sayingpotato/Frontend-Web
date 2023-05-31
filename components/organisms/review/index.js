b9863b8c56a5e9847471897bb78c2aad95b75836
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 02:33:35 +0900

    feat : update organisms/review/index.js (#37)

--- components/organisms/review/index.js
+++ components/organisms/review/index.js
@@ -1,13 +1,17 @@
+import { ReviewDiv } from './style';
+
 import PercentBlock from "@molecules/percentBlock"
 import ImageText from "@molecules/imageText"
 
 const Review = () => {
     return (
-        <PercentBlock>
-            <ImageText divClassName="basic" src="images/goldMedal.svg" alt="images/goldMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="oneInside"/>
-            <ImageText divClassName="basic" src="images/silverMedal.svg" alt="images/silverMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="twoInside"/>
-            <ImageText divClassName="basic" src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="threeInside"/>
-        </PercentBlock>
+        <ReviewDiv>
+            <PercentBlock>
+                <ImageText divClassName="basic" src="images/goldMedal.svg" alt="images/goldMedal.svg" text="리뷰명" textClass="reviewName" inlineBlockClass="oneInside" out="outside" />
+                <ImageText divClassName="basic" src="images/silverMedal.svg" alt="images/silverMedal.svg" text="리뷰명" textClass="reviewName" inlineBlockClass="twoInside" out="outside"/>
+                <ImageText divClassName="basic" src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" text="리뷰명" textClass="reviewName" inlineBlockClass="threeInside" out="outside"/>
+            </PercentBlock>
+        </ReviewDiv>
     );
 };
 
