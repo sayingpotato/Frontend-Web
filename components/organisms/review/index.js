ad643e716d8a2a979d4af8eedeccac8b5f749550
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 01:35:15 +0900

    feat : update organisms/review/index.js (#37) 기본 className 부여에 따른 변경

--- components/organisms/review/index.js
+++ components/organisms/review/index.js
@@ -4,9 +4,9 @@ import ImageText from "@molecules/imageText"
 const Review = () => {
     return (
         <PercentBlock>
-            <ImageText src="images/goldMedal.svg" alt="images/goldMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="oneInside"/>
-            <ImageText src="images/silverMedal.svg" alt="images/silverMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="twoInside"/>
-            <ImageText src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="threeInside"/>
+            <ImageText divClassName="basic" src="images/goldMedal.svg" alt="images/goldMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="oneInside"/>
+            <ImageText divClassName="basic" src="images/silverMedal.svg" alt="images/silverMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="twoInside"/>
+            <ImageText divClassName="basic" src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" text="리뷰명" TextClass="reviewName" inlineBlockClass="threeInside"/>
         </PercentBlock>
     );
 };
