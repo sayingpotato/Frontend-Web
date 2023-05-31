3929d06d728f9d3be460c6d41cd51ea9bf83b6b0
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 01:31:27 +0900

    feat : update molecules/imageText/style.js (#37) 기본 divClassName 부여

--- components/molecules/imageText/style.js
+++ components/molecules/imageText/style.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
 const StyledImageTextDiv = styled.div`
-    display: flex;
-    align-items: center;
-    gap : 3px;
+    &.basic {
+        display: flex;
+        align-items: center;
+        gap : 3px;
+    }
 
     &.storeInformationPostion {
         margin-left : 10px;
+        display: flex;
+        align-items: center;
+        gap : 3px;
     }
 `
 
