95b2355de709c23b6d47841576482d4898e91fba
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 01:31:57 +0900

    feat : update molecules/inlineText/style.js (#37) 기본 className 부여

--- components/molecules/inlineText/style.js
+++ components/molecules/inlineText/style.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 const StyledInlineTextDiv = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  padding: 0;
+  &.basic {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    padding: 0;
+  }
 `
 
 export { StyledInlineTextDiv };
