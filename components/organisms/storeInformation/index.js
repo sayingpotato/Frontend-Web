ba02e0f4ca823723286f6e8015ab933427d0c875
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 03:25:19 +0900

    feat : update organisms/storeInformation/index.js (#37) 아이콘 이미지 style 추가

--- components/organisms/storeInformation/index.js
+++ components/organisms/storeInformation/index.js
@@ -6,13 +6,13 @@ import ImageText from '@molecules/imageText';
 const StoreInformation = ({address, time, call, concent, rank, room}) => {
     return (
         <>
-        <ImageText src="images/position.svg" text={address} divClassName="storeInformationPostion" textClass="storeDetailInfo" />
+        <ImageText imageClassName="storeInfoIcon" src="images/position.svg" text={address} divClassName="storeInformationPostion" textClass="storeDetailInfo" />
             <StoreInformationDiv>
                 <TextInfoDiv>
-                    <ImageText src="images/clock.svg" text={time} textClass="storeDetailInfo" divClassName="basic" />
-                    <ImageText src="images/call.svg" text={call} textClass="storeDetailInfo" divClassName="basic" />
-                    <ImageText src="images/concent.svg" text={concent} textClass="storeDetailInfo" divClassName="basic" />
-                    <ImageText src="images/rank.svg" text={rank} textClass="storeDetailInfo" divClassName="basic" />
+                    <ImageText imageClassName="storeInfoIcon" src="images/clock.svg" text={time} textClass="storeDetailInfo" divClassName="basic" />
+                    <ImageText imageClassName="storeInfoIcon" src="images/call.svg" text={call} textClass="storeDetailInfo" divClassName="basic" />
+                    <ImageText imageClassName="storeInfoIcon" src="images/concent.svg" text={concent} textClass="storeDetailInfo" divClassName="basic" />
+                    <ImageText imageClassName="storeInfoIcon" src="images/rank.svg" text={rank} textClass="storeDetailInfo" divClassName="basic" />
                 </TextInfoDiv>
                 <Image src={room} alt={room} />
             </StoreInformationDiv>
