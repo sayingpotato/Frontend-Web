b7410c87097ef674a8a282eb75bd4a4595f0b0eb
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-01 01:34:55 +0900

    feat : update organisms/menuRanking/index.js (#37) 기본 className 부여에 따른 변경

--- components/organisms/menuRanking/index.js
+++ components/organisms/menuRanking/index.js
@@ -4,9 +4,9 @@ import ImageText from "@molecules/imageText"
 const MenuRanking = () => {
     return (
         <MenuRankingDiv>
-            <ImageText src="images/goldMedal.svg" alt="images/goldMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
-            <ImageText src="images/silverMedal.svg" alt="images/silverMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
-            <ImageText src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
+            <ImageText divClassName="basic" src="images/goldMedal.svg" alt="images/goldMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
+            <ImageText divClassName="basic" src="images/silverMedal.svg" alt="images/silverMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
+            <ImageText divClassName="basic" src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" text="메뉴명" TextClass="reviewMenuName"/>
         </MenuRankingDiv>
     );
 };
