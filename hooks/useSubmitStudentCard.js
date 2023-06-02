a4a23e37ce762dab0e4a62483c2bfaafa07dc028
knh6269 <knh6269@gmail.com>
2023-06-02 21:05:03 +0900

    feat: add useSubmitStudentCard hook (#44)

--- hooks/useSubmitStudentCard.js
+++ hooks/useSubmitStudentCard.js
@@ -1,5 +1,14 @@
-const useSubmitStudentCard = () => {
+import guestService from "services/guest.service"; 
 
+const useSubmitStudentCard = (image) => { 
+    
+    const submitStudentCard = async() => {
+        const data = await guestService.submitStudentCard(image);
+        return data;
+    }
+
+    return submitStudentCard;
 }
 
 export default useSubmitStudentCard;
+
