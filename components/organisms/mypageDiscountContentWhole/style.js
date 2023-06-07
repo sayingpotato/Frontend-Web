fbb61990aa4805004ed55b7d178e7de925a3c03c
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-07 22:50:39 +0900

    feat : add organisms/mypageDiscountContentWhole/style.js (#46) 최근 할인 받은 부분 더미 데이터 map 함수 이용

---
+++ components/organisms/mypageDiscountContentWhole/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const DiscountContentWholeDiv = styled.div`
+    display : flex;
+    flex-wrap: wrap;
+    padding-left : 20px;
+    padding-right : 20px;
+`
+
+const DiscountContent = styled.div`
+    width : 50%;
+    display : flex;
+    align-items: center;
+`
+
+export { DiscountContentWholeDiv, DiscountContent };
