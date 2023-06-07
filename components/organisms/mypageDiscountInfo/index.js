4867db63fc07eac7bf9ead485eb09134583c907a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-07 22:48:52 +0900

    feat : update organisms/mypageDiscountInfo/index.js (#46) 최근 받은 할인 더미 데이터 추가

--- components/organisms/mypageDiscountInfo/index.js
+++ components/organisms/mypageDiscountInfo/index.js
@@ -1,15 +1,14 @@
 import { MypageDiscountInfoDiv } from './style';
 
 import Title from '@molecules/title';
-import InlineText from '@molecules/inlineText';
-import ImageText from '@molecules/imageText';
+import MypageDiscountContentWhole from "@organisms/mypageDiscountContentWhole"
 
-const MypageDiscountInfo = () => {
+const MypageDiscountInfo = ({discountData}) => {
+
     return (
         <MypageDiscountInfoDiv>
-            <Title text="최근 받은 할인" />
-            <InlineText textOne="권낙현님" textTwo="할인금액" textOneClassName="mypageUserName" textTwoClassName="mypageMoneyTitle" />
-            <ImageText text="15000" src="images/coin.svg" alt="images/coin.svg" textClassName="mypageMoneyTotal" />
+            <Title text="최근 받은 할인" className="mypageDiscountTitle"/>
+            <MypageDiscountContentWhole discountData={discountData} />
         </MypageDiscountInfoDiv>
     );
 };
