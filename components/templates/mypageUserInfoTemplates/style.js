e7455c7c8b52658cab314e537d945bb3677045c4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-06-07 22:52:58 +0900

    feat : add templates/mypageUserInfoTemplates/style.js (#46) 마이페이지 상단의 고객 정보 요약 templates

---
+++ components/templates/mypageUserInfoTemplates/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MypageUserInfoDiv = styled.div`
+    background-color : #28469C;
+    height : 12vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
+
+export { MypageUserInfoDiv };
