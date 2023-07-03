5c5237259fbb3adaa6d98e995e7a4fe566494f45
knh6269 <knh6269@gmail.com>
2023-07-03 17:30:59 +0900

    feat: add store service (#43)

--- services/store.service.js
+++ services/store.service.js
@@ -1,17 +1,39 @@
-const getDailyDiscountStore = () => {
+import { storeApi, userApi } from "api"
 
+const getDailyDiscountStore = async() => {
+    try {
+        const data = await storeApi.getDailyDiscountStore();
+        return data;
+    } catch (error) {
+        throw error;
+    }
 }
 
-const getMapStoreList = () => {
-
+const getMapStoreList = async() => {
+    try {
+        const data = await storeApi.getMapStoreList();
+        return data;
+    } catch (error) {
+        throw error;
+    }
 }
 
-const getStoreDetail = () => {
-
+const getStoreDetail = async() => {
+    try {
+        const data = await storeApi.getStoreDetail();
+        return data;
+    } catch (error) {
+        throw error;
+    }
 }
 
-const getSurroundStoreList = () => {
-
+const getSurroundStoreList = async(latitude, longtitude) => {
+    try {
+        const data = await storeApi.getSurroundStoreList(latitude, longtitude);
+        return data;
+    } catch (error) {
+        throw error;
+    }
 }
 
 const storeService = {
