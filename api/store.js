f6b724922fa0ba8674e22dd74b1e21467a45501a
knh6269 <knh6269@gmail.com>
2023-07-03 17:30:34 +0900

    feat: add store api (#43)

--- api/store.js
+++ api/store.js
@@ -1,5 +1,42 @@
-const store = () => {
-    
+import fetcher from "./fetcher";
+
+const getDailyDiscountStore = async() => {
+    const {data} = await fetcher.get(
+        "api/v1/"
+    );
+
+    return data.data;
 }
 
-export default {store};
+const getMapStoreList = async() => {
+    const {data} = await fetcher.get(
+        
+    );
+
+    return data.data;
+}
+
+const getStoreDetail = async() => {
+    const {data} = await fetcher.get(
+        "api/v1/"
+    );
+
+    return data.data;
+}
+
+const getSurroundStoreList = async(latitude, longtitude) => {
+    const {data} = await fetcher.get(
+        `/api/v1/stores/nearby/list?latitude=${latitude}&longitude=${longtitude}&offset=0&limit=100`
+    );
+
+    return data.data;
+}
+
+const store = {
+    getDailyDiscountStore,
+    getMapStoreList,
+    getStoreDetail,
+    getSurroundStoreList
+}
+
+export default store;
