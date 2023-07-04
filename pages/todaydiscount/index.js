ae6308c43d1286c19e8f27bf32321a9223999c85
knh6269 <knh6269@gmail.com>
2023-07-04 15:33:30 +0900

    feat: add todaydiscount page (#43) 오늘의 할인페이지 api 연결 -> datas에다가 저장 해둠

--- pages/todaydiscount/index.js
+++ pages/todaydiscount/index.js
@@ -6,15 +6,15 @@ import Title from '@molecules/title'
 import TodayDiscountHeader from '@organisms/todayDiscountHeader'
 import TodayDiscountTemplate from '@templates/todayDiscountTemplate'
 
+import useGetDailyDiscountStore from '@hooks/useGetDailyDiscountStoreList';
 import { ContentWholeDiv, ContentDiv } from './style';
 
 const TodayDiscount = () => {
   const [nowDay, setNowDay] = useState('')
   const [prevDay, setPrevDay] = useState('')
   const [nextDay, setNextDay] = useState('')
-
   const [clickValue, setClickValue] = useState('food')
-
+  const [datas, setData] = useState(null);  
   const days = [
     '일요일',
     '월요일',
@@ -23,12 +23,23 @@ const TodayDiscount = () => {
     '목요일',
     '금요일',
     '토요일',
-  ]
+  ];
 
-  useEffect(() => {
-    setNowDay(date())
+  const days2 = [
+    'SUN', 'MON','MON', 'WED', 'THU', 'FRI', 'SAT'
+  ];
 
-    const idx = days.indexOf(date())
+  const getDailyDiscountStore = useGetDailyDiscountStore(days2[days.indexOf(nowDay)]);
+  const dates = date();
+
+  useEffect(() => { 
+    setData(getDailyDiscountStore);
+  }, [getDailyDiscountStore]); 
+
+  useEffect(() => {  
+    setNowDay(dates)
+
+    const idx = days.indexOf(dates)
 
     if (idx === 0) {
       setPrevDay('토요일')
@@ -42,7 +53,7 @@ const TodayDiscount = () => {
       setNextDay(days[idx + 1])
       setPrevDay(days[idx - 1])
     }
-  }, [date()])
+  }, [dates])
 
   let data = [
     {
@@ -123,6 +134,13 @@ const TodayDiscount = () => {
   return (
     <>
       <Title text="오늘의 할인"/>
+      <div>
+        {datas && datas.map((item, index) => {
+          return (
+            <div key = {index}>{item.name}</div>
+          )
+        })}
+      </div>
       <TodayDiscountHeader FoodOnClick={typeFoodClick} CafeOnClick={typeCafeClick} 
         state={clickValue}
         prevDay={`${prevDay}`} nowDay={`${nowDay}`} nextDay={`${nextDay}`}
