2fa000d22b3e0ba35f72213da0f4fe738bef890b
knh6269 <knh6269@gmail.com>
2023-07-05 16:16:05 +0900

    feat: modify useGetDailyDiscountStoreList hooks (#43) 오늘의 할인 가게 가져오기 훅 수정

--- hooks/useGetDailyDiscountStoreList.js
+++ hooks/useGetDailyDiscountStoreList.js
@@ -3,16 +3,17 @@ import storeService from "services/store.service";
 
 const useGetDailyDiscountStore = (date) => {
     const [data, setData] = useState("");
-
+    
     useEffect(() => {
-        if (date === undefined) return ;
         const getStore = async() => {
-            const fetchData = await storeService.getDailyDiscountStore(date); 
-            setData(fetchData);
+            if (date !== undefined) {
+                const fetchData = await storeService.getDailyDiscountStore(date); 
+                setData(fetchData);
+            }
         }
         getStore();
     }, [date]);
-
+    
     return data;
 }
 
