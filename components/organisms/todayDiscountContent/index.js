aca07ac24fb1d09e32f7eb523873a5234dc9aaa7
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-09-16 11:25:30 +0900

    feat : update organisms/todayDiscountContent/index.js (#54) 클릭 함수 추가

--- components/organisms/todayDiscountContent/index.js
+++ components/organisms/todayDiscountContent/index.js
@@ -2,10 +2,10 @@ import { TodayDiscountContentDiv } from './style';
 
 import ImageTitle from '@molecules/imageTitle'
 
-const TodayDiscountContent = ({src, alt, text}) => {
+const TodayDiscountContent = ({src, alt, text, onClick}) => {
     return (
         <TodayDiscountContentDiv>
-            <ImageTitle imageClassName='todayDiscountLogoImage' textClassNameName='todayDiscountLogoName' src={src} alt={alt} text={text} />
+            <ImageTitle onClick={onClick} imageClassName='todayDiscountLogoImage' textClassNameName='todayDiscountLogoName' src={src} alt={alt} text={text} />
         </TodayDiscountContentDiv>
     );
 };
