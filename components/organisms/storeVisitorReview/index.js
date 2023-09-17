4e8bce29bd4058c59a3ddc2cabb139e9cd5b1217
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-09-17 12:42:36 +0900

    feat : update organisms/storeVisitorReview/index.js (#54) 리뷰 개수 css

--- components/organisms/storeVisitorReview/index.js
+++ components/organisms/storeVisitorReview/index.js
@@ -6,17 +6,15 @@ import InlineText from '@molecules/inlineText';
 
 const StoreVisitorReview = ({reviewData}) => {
     return (
-        <>
-            <StoreVisitorReviewDiv>
-                    <InlineText divClassName="basic" textOne="방문자 리뷰" textTwo={reviewData.allNum} textOneClassName="storeDetailMiniTitle" textTwoClassName="storeDetailReviewTotal" />
-                    <PercentBlock>
-                        <ImageText text={"커피가 맛있어요"} textClassName="reviewName" inlineBlockClass="oneVisitorReviewInside" out="visitorReviewOutside" reviewCount={reviewData.greatBeverageNum} />
-                        <ImageText text={"가성비가 좋아요"} textClassName="reviewName" inlineBlockClass="twoVisitorReviewInside" out="visitorReviewOutside" reviewCount={reviewData.greatCoffeeNum} />
-                        <ImageText text={"화장실이 좋아요"} textClassName="reviewName" inlineBlockClass="threeVisitorReviewInside" out="visitorReviewOutside" reviewCount={reviewData.greatFoodNum} />
-                        <ImageText text={"분위기가 좋아요"} textClassName="reviewName" inlineBlockClass="fourVisitorReviewInside" out="visitorReviewOutside" reviewCount={reviewData.manyOutletNum} />
-                    </PercentBlock>
-            </StoreVisitorReviewDiv>
-        </>
+        <StoreVisitorReviewDiv>
+                <InlineText divClassName="basic" textOne="방문자 리뷰" textTwo={reviewData.allNum} textOneClassName="storeDetailMiniTitle" textTwoClassName="storeDetailReviewTotal" />
+                <PercentBlock>
+                    <ImageText text={"커피가 맛있어요"} textClassName="reviewName" inlineBlockClass="oneVisitorReviewInside" out="visitorReviewOutside" reviewCountText={reviewData.greatBeverageNum} reviewCountTextClassName="reviewCount" />
+                    <ImageText text={"가성비가 좋아요"} textClassName="reviewName" inlineBlockClass="twoVisitorReviewInside" out="visitorReviewOutside" reviewCountText={reviewData.manyOutletNum} reviewCountTextClassName="reviewCount" />
+                    <ImageText text={"화장실이 좋아요"} textClassName="reviewName" inlineBlockClass="threeVisitorReviewInside" out="visitorReviewOutside" reviewCountText={reviewData.greatFoodNum} reviewCountTextClassName="reviewCount" />
+                    <ImageText text={"분위기가 좋아요"} textClassName="reviewName" inlineBlockClass="fourVisitorReviewInside" out="visitorReviewOutside" reviewCountText={reviewData.greatFoodNum} reviewCountTextClassName="reviewCount" />
+                </PercentBlock>
+        </StoreVisitorReviewDiv>
     );
 };
 
