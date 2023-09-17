e7d315a48b9cb8ee01306e3d23ae440e2c96e82b
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-09-17 12:42:15 +0900

    feat : update molecules/percentBlock/index.js (#54) 리뷰 개수 css

--- components/molecules/percentBlock/index.js
+++ components/molecules/percentBlock/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyledPercentBlockDiv } from './style';
 import InlineBlock from "@atoms/inlineBlock"
 import ImageText from "@molecules/imageText"
+import Text from "@atoms/text"
 
 const PercentBlock = ({ children }) => {
   return (
@@ -11,6 +12,7 @@ const PercentBlock = ({ children }) => {
         <InlineBlock className={child.props.out}>
           <InlineBlock className={`${child.props.inlineBlockClass}`}>
             <ImageText divClassName="basic" src={child.props.src} alt={child.props.alt} text={child.props.text} textClassName={child.props.textClassName}/>
+            <Text className={child.props.reviewCountTextClassName} text={child.props.reviewCountText} />
           </InlineBlock>
         </InlineBlock>
       ))}
