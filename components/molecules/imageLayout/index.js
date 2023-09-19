5f360810e3bbe0f5645a076dec7120427260ca3e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-09-20 00:01:39 +0900

    feat : update molecules/imageLayout/index.js (#61) API 가게 이미지

--- components/molecules/imageLayout/index.js
+++ components/molecules/imageLayout/index.js
@@ -2,18 +2,18 @@ import { StyledImageLayoutWholeDiv, StyledImageLayoutOneDiv, StyledImageLayoutTw
 
 import Image from "@atoms/image";
 
-const imageLayout = ({ className }) => {
+const imageLayout = ({ imageSrc, className }) => {
     return (
         <StyledImageLayoutWholeDiv>
             <StyledImageLayoutOneDiv>
-                <Image src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" className="imageLayoutImage"/>
+                <Image src={imageSrc.mainImg} alt={imageSrc.mainImg} className="imageLayoutImage"/>
             </StyledImageLayoutOneDiv>
             <StyledImageLayoutTwoDiv>
-                <Image src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" className="imageLayoutMiddleImage"/>
-                <Image src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" className="imageLayoutMiddleImage"/>
+                <Image src={imageSrc.subImg1} alt={imageSrc.subImg1} className="imageLayoutMiddleImage"/>
+                <Image src={imageSrc.subImg2} alt={imageSrc.subImg2} className="imageLayoutMiddleImage"/>
             </StyledImageLayoutTwoDiv>
             <StyledImageLayoutThreeDiv>
-                <Image src="images/bronzeMedal.svg" alt="images/bronzeMedal.svg" className="imageLayoutImage"/>
+                <Image src={imageSrc.subImg3} alt={imageSrc.subImg3} className="imageLayoutImage"/>
             </StyledImageLayoutThreeDiv>
         </StyledImageLayoutWholeDiv>
     );
