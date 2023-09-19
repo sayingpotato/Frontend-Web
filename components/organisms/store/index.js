83d56343491cf92b4ac1d0b9c9cf099eb0795f1c
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-09-20 00:03:21 +0900

    feat : update organisms/store/index.js (#61) API props 데이터로 전달

--- components/organisms/store/index.js
+++ components/organisms/store/index.js
@@ -6,17 +6,17 @@ import InlineText from "@molecules/inlineText"
 import MenuRanking from '@organisms/menuRanking';
 import Review from '@organisms/review';
 
-const Store = ({storeName, imageSrc, minDiscount, maxDisount}) => {
+const Store = ({storeName, imageSrc, minDiscount, maxDisount, menuTopArr, reviewTopArr}) => {
     return (
         <div>
-            <ImageLayout />
+            <ImageLayout imageSrc={imageSrc} />
             <StyledStoreInfoDiv>
                 <Text text={storeName} className="storeName" />
                 <InlineText divClassName="basic" textOne={minDiscount} textOneClassName="minDiscount" textTwo={maxDisount} textTwoClassName="maxDiscount"/>
             </StyledStoreInfoDiv>
             <StyledReviewInfoDiv>
-                <MenuRanking />
-                <Review />
+                <MenuRanking menuTopArr={menuTopArr} />
+                <Review reviewTopArr={reviewTopArr} />
             </StyledReviewInfoDiv>
         </div>
     );
