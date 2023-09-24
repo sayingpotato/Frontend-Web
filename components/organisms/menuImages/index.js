9c03e4630977515b54fc57c9022c7708d26d93b8
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-09-25 02:06:50 +0900

    feat : update organisms/menuImages/index.js (#66) 주문하기 전 메뉴 이미지 누를 경우 토글 형식

--- components/organisms/menuImages/index.js
+++ components/organisms/menuImages/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { MenuImagesDiv, MenuWholeImagesDiv, MenuImagesContentDiv, ImagesDiv } from './style';
 
 import Image from "@atoms/image"
@@ -5,13 +7,28 @@ import MenuInfo from '@organisms/menuInfo'
 import Text from "@atoms/text"
 
 const MenuImages = ({menuData}) => {
+    const [selectedMenuIds, setSelectedMenuIds] = useState([]);
+
+    const toggleMenu = (itemId) => {
+        setSelectedMenuIds((prevSelectedMenuIds) => {
+        if (prevSelectedMenuIds.includes(itemId)) {
+            return prevSelectedMenuIds.filter((id) => id !== itemId);
+        } else {
+            return [...prevSelectedMenuIds, itemId];
+        }
+        });
+    };
     return (
         <>
             <MenuImagesDiv>
                 <Text text="메뉴판" className="storeDetailMiniTitle" />
                 <MenuWholeImagesDiv>
                     {menuData.map((item, index) => (
-                        <MenuImagesContentDiv key={item.id}>
+                        <MenuImagesContentDiv
+                            key={item.id}
+                            onClick={() => toggleMenu(item.id)}
+                            isSelected={selectedMenuIds.includes(item.id)}
+                        >
                             <ImagesDiv>
                             <Image src={item.img} alt={item.img} className="menuImage"/>
                                 <MenuInfo name={item.name} price={item.price} rank={item.rank} />
