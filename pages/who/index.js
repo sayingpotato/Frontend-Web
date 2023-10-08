b862469b947ca4242cd37a1b8934444ee97936ca
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-08 22:50:11 +0900

    feat : add who/index.js (#70)

--- pages/who/index.js
+++ pages/who/index.js
@@ -1,21 +1,37 @@
 import { useEffect, useState, useRef, use } from 'react'
-import router from "next/router";
+import Router, { useRouter } from 'next/router'
 
 import Text from "@atoms/text"
-import { ContentDiv } from '../../styles/who/style'
+import { ContentWholeDiv, ContentDiv } from '../../styles/who/style'
 
 import ImageTitle from '@molecules/imageTitle'
 
 const Who = () => {
+    const onClickTodayDiscountValue = () => {
+        Router.push({
+          pathname: '/storedetail',
+          query: {  },
+        })
+    }
+
+    const onClickRegister = () => {
+        Router.push({
+          pathname: '/register/auth/self',
+          query: {  },
+        })
+    }
+    
     return (
         <div>
-            <Text text="누구신가요?" className="whoText"></Text>
-            <ContentDiv>
-                <ImageTitle imageClassName='whoLogo' textClassNameName='whoAreText' src="/images/whoOwner.png" alt="/images/whoOwner.png" text="점주님" />
-            </ContentDiv>
-            <ContentDiv>
-                <ImageTitle imageClassName='whoLogo' textClassNameName='whoAreText' src="/images/whoOwner.png" alt="/images/whoOwner.png" text="점주님" />
-            </ContentDiv>
+            <Text text="누구신가요?" className="whoText"></Text>    
+            <ContentWholeDiv>
+                <ContentDiv>
+                    <ImageTitle onClick={onClickTodayDiscountValue} imageClassName='whoLogo' textClassName='whoAreText' src="/images/whoOwner.png" alt="/images/whoOwner.png" text="점주님" />
+                </ContentDiv>
+                <ContentDiv>
+                    <ImageTitle onClick={onClickRegister} imageClassName='whoLogo' textClassName='whoAreText' src="/images/whoPotato.png" alt="/images/whoPotato.png" text="감자" />
+                </ContentDiv>
+            </ContentWholeDiv>
         </div>
         
     )
