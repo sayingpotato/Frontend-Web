78f56c4207b22ea5815b158af431c3ba34d802c4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-08 17:30:15 +0900

    feat : add hooks/useSubmitReview.js (#66) 리뷰 API 추가

---
+++ hooks/useSubmitReview.js
@@ -0,0 +1,17 @@
+import storeService from "services/store.service"; 
+import { useRecoilValue } from "recoil";
+import { ReviewData } from "@utils/recoil/atom";
+
+const useSubmitReview = () => { 
+    const reviewData = useRecoilValue(ReviewData);
+
+    const submitReview = async() => {
+        const data = await storeService.submitReview(reviewData);
+        return data;
+    }
+
+    return submitReview;
+}
+
+export default useSubmitReview;
+
