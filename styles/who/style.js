98d6515a72bc78f8ea8c35b036e01f2bcedeac35
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-08 22:47:32 +0900

    feat : styles/who/style.js (#70)

---
+++ styles/who/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const ContentDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  width : 200px;
+  height : 200px;
+`
+
+export { ContentDiv }
