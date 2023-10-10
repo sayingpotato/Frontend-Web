d51f494482ab4aefbf37faa554980848cc92cdf0
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-11 00:12:14 +0900

    feat : add templates/mystoreTemplate/style.js (#76)

---
+++ components/templates/mystoreTemplate/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const WholeDiv = styled.div`
+    padding : 5px;
+    border-radius : 10px;
+    margin-left : 10px;
+    margin-right : 10px;
+    box-shadow: 1px 1px 1px 1px grey inset;
+    height : 450px;
+`
+
+export { WholeDiv }
