fe11d91e912630d393ebd6bc1e5884681a0a1d38
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-11 00:09:33 +0900

    feat : update hooks/useOwnerLogin.js (#76) owner 상태 주기

--- hooks/useOwnerLogin.js
+++ hooks/useOwnerLogin.js
@@ -1,13 +1,15 @@
 import guestService from "services/guest.service";
 import { useSetRecoilState } from "recoil";
-import { LoginOwnerStatus } from "@utils/recoil/atom";
+import { LoginOwnerStatus, WhoStatus } from "@utils/recoil/atom";
 import Router from "next/router";
 import { setCookie } from "@utils/cookie";
 
 const useOwnerLogin = (id, pw) => {
     const setLoginStatus = useSetRecoilState(LoginOwnerStatus);
+    const setWhoStatus = useSetRecoilState(WhoStatus);
 
     const login = async () => {
+        setWhoStatus("");
         const { data } = await guestService.ownerLogin({
             loginId: id,
             password: pw,
@@ -21,6 +23,7 @@ const useOwnerLogin = (id, pw) => {
         Router.push("map");
 
         setLoginStatus(true);
+        setWhoStatus("owner");
         setCookie('accessToken', data.accessToken, {
             path: '/'
         }); 
