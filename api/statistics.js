5dde260aa6ccf55d889e19461cef54225c88642a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-18 01:14:01 +0900

    feat : update api/statics.js (#83) Daily Revenue API 추가

--- api/statistics.js
+++ api/statistics.js
@@ -7,8 +7,16 @@ const getDailyProfit = async(id) => {
     return data.data;
 }
 
+const getDailyRevenue = async(id) => {
+    const {data} = await fetcher.get(
+        `api/v1/statistics/daily/revenue?storeId=${id}`
+    );
+    return data.data;
+}
+
 const statistics = {
-    getDailyProfit
+    getDailyProfit,
+    getDailyRevenue
 }
 
 export default statistics;
