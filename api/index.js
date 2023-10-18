2239791fe18e22e570e4fa0f76a19685b1ff227b
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-18 22:49:12 +0900

    feat : update api/index.js (#83) prediction API 추가

--- api/index.js
+++ api/index.js
@@ -2,3 +2,4 @@ export {default as guestApi} from "./guest";
 export {default as userApi} from "./user";
 export {default as storeApi} from "./store";
 export {default as statisticsApi} from "./statistics";
+export {default as predictionApi} from "./prediction";
