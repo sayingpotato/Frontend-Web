314cb7c4eeb854aad51e2e3264ae8eb367961c22
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-18 23:42:35 +0900

    feat : update services/prediction.service.js (#83) 예측 API 추가

--- services/prediction.service.js
+++ services/prediction.service.js
@@ -1,7 +1,46 @@
 import {predictionApi} from "api"
 
+const getDailyItem = async(id) => {
+    try {
+        const data = await statisticsApi.getWeekItem(id);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getDailyItemTomorrow = async(id) => {
+    try {
+        const data = await statisticsApi.getWeekItem(id);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getProfit = async(id) => {
+    try {
+        const data = await statisticsApi.getWeekItem(id);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getProfitTomorrow = async(id) => {
+    try {
+        const data = await statisticsApi.getWeekItem(id);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
 const predictionService = {
-    
+    getDailyItem,
+    getDailyItemTomorrow,
+    getProfit,
+    getProfitTomorrow
 }
 
 export default predictionService;
