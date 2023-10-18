9d14d62c67b6c4b1a2f02b8db51024f48351e695
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-18 23:43:11 +0900

    feat : update templates/mystoreTemplate/index.js (#83) 페이지 버튼 추가

--- components/templates/mystoreTemplate/index.js
+++ components/templates/mystoreTemplate/index.js
@@ -2,6 +2,8 @@ import Router, { useRouter } from 'next/router';
 
 import Text from '@components/atoms/text';
 import Image from '@components/atoms/image';
+import Button from '@components/atoms/button';
+
 import { WholeDiv } from './style';
 
 const MystoreTemplate = ({data}) => {
@@ -12,12 +14,32 @@ const MystoreTemplate = ({data}) => {
             query: { id: data.storeId, storeName: data.name },
         })
     }
+
+    const router = useRouter();
+
+    const statisticsOnClick = () => {
+        router.push({
+            pathname: '/statistics',
+            query: { id: data.storeId },
+        })
+    }
+
+    const predictionOnClick = () => {
+        router.push({
+            pathname: '/predictio',
+            query: { id: data.storeId },
+        })
+    }
     
     return (
-        <WholeDiv key={data.storeId} onClick={mystoreDetailButton}>
-            <Text className="mystoreName" text={data.name} />
-            <Text className="mystoreDes" text={data.description} />
-            <Image className="mystoreImage" src={data.thumbnail} />
+        <WholeDiv key={data.storeId}>
+            <div onClick={mystoreDetailButton}>
+                <Text className="mystoreName" text={data.name} />
+                <Text className="mystoreDes" text={data.description} />
+                <Image className="mystoreImage" src={data.thumbnail} />
+            </div>
+            <Button text="통계 내역" onClick={statisticsOnClick} />
+            <Button text="예측" onClick={predictionOnClick} />
         </WholeDiv>
   );
 };
