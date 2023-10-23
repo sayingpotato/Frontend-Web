16011fc6f7883f1ae1cc42bcd1b0c62885e2e1c2
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-24 02:51:05 +0900

    feat : update pages/prediction/index.js (#83) 오늘, 내일 버튼에 따른 페이지 변화

--- pages/prediction/index.js
+++ pages/prediction/index.js
@@ -1,21 +1,55 @@
 import Router, { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import DailyItem from '@components/organisms/dailyItem';
 import DailyItemTomorrow from '@components/organisms/dailyItemTomorrow';
 import Profit from '@components/organisms/profit';
 import ProfitTommorow from '@components/organisms/profitTomorrow';
+import Title from '@components/molecules/title';
+import Line from '@components/line';
+import InlineButton from "@molecules/inlineButton"
 
 const Prediction = () => {
 
     const router = useRouter();
     const id = router.query.id;
 
+    const [type, setType] = useState("todayPrediction");
+    const [titleText, setTitleText] = useState("오늘 우리 가게는...");
+
+    const todayPredictionClick = () => {
+        setType("todayPrediction")
+        setTitleText("오늘 우리 가게는...")
+    }
+
+    const tommorowPredictionClick = () => {
+        setType("tommorowPrediction")
+        setTitleText("내일 우리 가게는...")
+    }
+
     return (
         <>
-            <DailyItem id={id} />
-            <DailyItemTomorrow id={id} />
-            <Profit id={id} />
-            <ProfitTommorow id={id} />
+            <Title text={titleText} />
+            <InlineButton textOne="식당" textTwo="카페" 
+                textOneClassName="typeTodayPrediction" textTwoClassName="typeTommorowPrediction"
+                textOneOnClick={todayPredictionClick} textTwoOnClick={tommorowPredictionClick}
+                state={type}
+                divClassName="center"
+            />
+
+            {type === "todayPrediction" ? (
+                <>
+                    <DailyItem id={id} />
+                    <Line />
+                    <Profit id={id} />
+                </>
+            ) : (
+                <>
+                    <DailyItemTomorrow id={id} />
+                    <Line />
+                    <ProfitTommorow id={id} />
+                </>
+            )}
         </>
     )
 }
