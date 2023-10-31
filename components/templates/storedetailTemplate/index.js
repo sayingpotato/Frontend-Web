877b14e149543c7112e4f778ed4558494ac835a3
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-10-31 23:19:16 +0900

    feat : update templates/storedetailTemplate/index.js (#85) 가게 세부 정보 할인 정보 강조

--- components/templates/storedetailTemplate/index.js
+++ components/templates/storedetailTemplate/index.js
@@ -5,6 +5,7 @@ import Line from "@atoms/line"
 import Title from "@molecules/title"
 import StoreInformation from "@organisms/storeInformation"
 import StoreVisitorReview from "@organisms/storeVisitorReview"
+import DiscountInformation from '@organisms/discountInformation';
 import StoreImages from "@organisms/storeImages"
 import MenuImages from "@organisms/menuImages"
 
@@ -16,6 +17,8 @@ const StorelistTemplate = ({data}) => {
             <Title text={data.name} />
             <Line />
             <StoreInformation address={data.address.roadAddr} time={data.storeOperationHours} call={data.phone} concent={data.outletNum} rank={"1위 무질서도"} room={data.tableImg} />
+            <Line />
+            <DiscountInformation discountData={data.discounts} />
             <Line />
             <StoreVisitorReview reviewData={data.reviewResult}/>
             <Line />
