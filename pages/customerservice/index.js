e52ffe3872e8c40937be312a9eb854effc1707c0
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-11-02 21:32:13 +0900

    feat : add pages/customerservice/index.js (#85) 고객센터 페이지

---
+++ pages/customerservice/index.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import router from "next/router";
+import { useRecoilState } from "recoil";
+
+import Text from "@components/atoms/text";
+
+const Customerservice = () => {
+    return (
+        <>
+            <h1>111</h1>
+        </>
+    )
+}
+
+export default Customerservice
