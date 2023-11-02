d46068a97da0a26867060ce4f75d96aa682f7abb
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-11-02 22:10:15 +0900

    feat : update organisms/mypageDiscountContentWhole/index.js (#90) 마이페이지 돈 단위 수정

--- components/organisms/mypageDiscountContentWhole/index.js
+++ components/organisms/mypageDiscountContentWhole/index.js
@@ -12,7 +12,7 @@ const MypageDiscountInfo = ({discountDataDetail}) => {
         return (
             <DiscountContent key={index}>
                 <Image src="images/coin.svg" alt="돈 사진"/>
-                <InlineText divClassName="discountContentValue" textOne={item.storeName} textTwo={item.discountPrice} textOneClassName="discountContentName" textTwoClassName="discountContentMoney" />
+                <InlineText divClassName="discountContentValue" textOne={item.storeName} textTwo={item.discountPrice.toLocaleString()} textOneClassName="discountContentName" textTwoClassName="discountContentMoney" />
             </DiscountContent>
         )})}
         </DiscountContentWholeDiv>
