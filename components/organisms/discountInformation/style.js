c41be46682e65096ebf4b0226c47416d9539d78a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-11-02 20:17:29 +0900

    feat : update organisms/discountInformation/style.js (#85) 할인 정보 css 수정

--- components/organisms/discountInformation/style.js
+++ components/organisms/discountInformation/style.js
@@ -2,19 +2,21 @@ import styled from 'styled-components';
 
 const Div = styled.div`
     display : flex;
-    gap : 40px;
+    gap : 10px;
     margin-left : 10px;
     margin-right : 10px;
+    overflow-x: scroll;
 `
 
 const ContentDiv = styled.div`
     border: 1px solid black;
     border-radius: 5px;
-    width : 100px;
     text-align : center;
     display : flex;
     flex-direction : column;
     gap : 3px;
+    padding : 8px;
+    height : 70px;
 `
 
 export { Div, ContentDiv };
