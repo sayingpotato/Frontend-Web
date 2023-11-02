323c04d1c6a8bd0eb04f74d36fbd51f9a1b36e84
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-11-02 20:05:23 +0900

    feat : update pages/mypage/index.js (#85) 내가 할인 받은 항목 수정

--- pages/mypage/index.js
+++ pages/mypage/index.js
@@ -4,24 +4,29 @@ import Title from '@molecules/title';
 import MypageContentInfoTemplate from '@components/templates/mypageContentInfoTemplate';
 import useGetNickName from '@hooks/useGetNickName';
 import useGetDiscountTotal from '@hooks/useGetDiscountTotal';
+import useGetDiscountDetail from '@hooks/useGetDiscountDetail';
 
 const Mypage = () => {
 
   const [userInfo, setUserInfo] = useState("");
   const [discountInfo, setDiscountInfo] = useState("");
+  const [discountDetail, setDiscountDetail] = useState("");
 
   const getNickName = useGetNickName();
   const getDiscountTotal = useGetDiscountTotal();
+  const getDiscountDetail = useGetDiscountDetail();
 
   useEffect(() => { 
     setUserInfo(getNickName); 
     setDiscountInfo(getDiscountTotal);
-  },[getNickName, getDiscountTotal]);
+    setDiscountDetail(getDiscountDetail)
+
+  },[getNickName, getDiscountTotal, getDiscountDetail]);
 
   return (
     <>
       <Title text="마이페이지" />
-      <MypageContentInfoTemplate name={userInfo.nickname} discountData={discountInfo}/>
+      <MypageContentInfoTemplate name={userInfo.nickname} discountData={discountInfo} discountDataDetail={discountDetail} />
     </>
   )
 }
