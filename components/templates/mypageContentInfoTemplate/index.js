fd7136ea314723d09782cb07850994d21be0936a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-11-03 02:20:04 +0900

    feat : update templates/mypageContentInfoTempalte/index.js (#90) 내 정보 페이지

--- components/templates/mypageContentInfoTemplate/index.js
+++ components/templates/mypageContentInfoTemplate/index.js
@@ -7,20 +7,20 @@ import MypageLinkButtons from '@organisms/mypageLinkButtons';
 import { useRecoilValue } from "recoil";
 import { WhoStatus } from "@utils/recoil/atom";
 
-const MypageContentInfoTemplate = ({name, discountData, discountDataDetail}) => {
+const MypageContentInfoTemplate = ({info, discountData, discountDataDetail}) => {
   const whoStatus = useRecoilValue(WhoStatus);
     
   return (
     <>
       {
-        whoStatus === "customer" ? <MypageUserInfo name={name} totalMoney={discountData.totalDiscountPrice}/> : <MypageUserInfo name={name} totalMoney={0}/>
+        whoStatus === "customer" ? <MypageUserInfo name={info.nickname} totalMoney={discountData.totalDiscountPrice}/> : <MypageUserInfo name={info.nickname} totalMoney={0}/>
       }
       
       <MypageContentInfoTemplateDiv>
           {
             whoStatus === "customer" ? <MypageDiscountInfo discountDataDetail={discountDataDetail} /> : null
           }
-          <MypageLinkButtons />
+          <MypageLinkButtons info={info} />
       </MypageContentInfoTemplateDiv>
     </>
       
