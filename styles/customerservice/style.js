107ffc332364130d2a6a8c26ff2a6c2383fd6a64
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-11-02 22:40:20 +0900

    feat : update styles/customerservice/style.js (#90) 고객센터 페이지

---
+++ styles/customerservice/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+    text-align : center;
+`;
+
+const WholeDiv = styled.div`
+    display : flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`
+
+const TelDiv = styled.div`
+    display : flex;
+    flex-direction: column;
+    margin : 0;
+`
+
+export { Div, WholeDiv, TelDiv }
