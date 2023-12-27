6548f33a5301f987aae3b968e0144d2614f86f6e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-28 00:12:13 +0900

    feat : update organisms/memberTitle/style.js (#94)

--- components/organisms/memberTitle/style.js
+++ components/organisms/memberTitle/style.js
@@ -4,16 +4,14 @@ const MemberTitleDiv = styled.div`
     background-color: #28469C;
 `;
 
-const TitleTop = styled.div`
-    padding-left : 20px ;
-`;
-
 const TitleUnder = styled.div`
     display: flex;
-    padding: 4px 20px 20px 24px;
+    padding: 4px 20px 0px 24px;
     color: #FFFFFF;
     opacity: 0.6;
     box-shadow: 0 4px 4px 0 gray;
+    justify-content : space-between;
+    align-items: flex-end;
 `;
 
-export {MemberTitleDiv, TitleTop, TitleUnder};
+export {MemberTitleDiv, TitleUnder};
