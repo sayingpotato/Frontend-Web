fddac06de0936f65d8cacc84c01499d2d7f638fd
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-27 23:22:08 +0900

    feat : add components/organisms/memberTitle/style.js (#94)

---
+++ components/organisms/memberTitle/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const MemberTitleDiv = styled.div`
+    background-color: #28469C;
+`;
+
+const TitleTop = styled.div`
+    padding-left : 20px ;
+`;
+
+const TitleUnder = styled.div`
+    display: flex;
+    padding: 4px 20px 20px 24px;
+    color: #FFFFFF;
+    opacity: 0.6;
+    box-shadow: 0 4px 4px 0 gray;
+`;
+
+export {MemberTitleDiv, TitleTop, TitleUnder};
