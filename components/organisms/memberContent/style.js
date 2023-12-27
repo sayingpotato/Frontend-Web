33745a605b32766a24b0ad6d65948fb7bb0a9233
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-28 00:11:31 +0900

    feat : update organisms/memberContent/style.js (#94)

--- components/organisms/memberContent/style.js
+++ components/organisms/memberContent/style.js
@@ -5,6 +5,9 @@ const Main = styled.div`
     flex-direction: column;
     gap: 12px;
     justify-content: center;
+    align-items: center;
+    margin-top:28px;
+    flex-grow : 1;
 `;
 
 export { Main };
