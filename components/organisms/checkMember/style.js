446b42f0d9ac787e802d9bb9f4985e7c88f4a67e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-27 23:21:28 +0900

    feat : add components/organisms/checkMember/style.js (#94) 회원 여부 확인 및 회원가입 버튼

---
+++ components/organisms/checkMember/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const CheckMemberDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-grow: 1;
+`;
+
+export { CheckMemberDiv };
