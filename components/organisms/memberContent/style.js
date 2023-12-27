bb2918abc8fd444a4d82d557401d6acc035719d2
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-27 23:21:56 +0900

    feat : add components/organisms/memberContent/style.js (#94)

---
+++ components/organisms/memberContent/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    justify-content: center;
+`;
+
+export { Main };
