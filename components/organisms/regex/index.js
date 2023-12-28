b5bc6dcb3709541ccdbfa867df60e0826889670f
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-29 00:53:23 +0900

    feat : add organisms/regex/index.js (#94) 정규화 상태 값 체크 컴포넌트 생성

---
+++ components/organisms/regex/index.js
@@ -0,0 +1,51 @@
+import { RegexComponent, RegexView } from "./style"
+
+import Text from "@atoms/text";
+import { 
+    isEnglish, 
+    isNumber, 
+    isMoreThan,
+    isSpecialCharacter,
+    isKoreanEnglish } from "@hooks/regex";
+import { useEffect, useState } from "react";
+import { 
+    ID_LENGTH, 
+    PW_LENGTH, 
+    NickName_LENGTH } from "@constants/register";
+
+export default function Regex(props){
+    const [isValid, setIsValid] = useState(false);
+    
+    const regexCheckdata = {
+        'English' : {title : '영어', state : isValid},
+        'Number' : {title : '숫자', state : isValid},
+        'Eight' : {title : '8자 이상', state : isValid},
+        'Six' : {title : '6자 이상', state : isValid},
+        'Two' : {title : '2자 이상', state : isValid},
+        'SpecialCharacter' : {title : '특수문자', state : isValid},
+        'KoreanEnglish' : {title : '한글/영어', state : isValid}
+    };
+
+    useEffect(() => {
+        props.onValidityChange(props.action, isValid);
+    },[isValid, props.action, props])
+    
+    useEffect(() => {
+        switch (props.action) {
+            case 'English': setIsValid(isEnglish(props.input)); break;
+            case 'Number': setIsValid(isNumber(props.input)); break;
+            case 'Two': setIsValid(isMoreThan(props.input, NickName_LENGTH)); break;
+            case 'Six': setIsValid(isMoreThan(props.input, ID_LENGTH)); break;
+            case 'Eight': setIsValid(isMoreThan(props.input, PW_LENGTH)); break;
+            case 'SpecialCharacter': setIsValid(isSpecialCharacter(props.input)); break;
+            case 'KoreanEnglish': setIsValid(isKoreanEnglish(props.input)); break;
+        }
+    },[props.input, props.onValidityChange, props.action])
+    
+    return (
+        <RegexComponent>
+            <RegexView state={regexCheckdata[props.action].state}/>
+            <Text state={regexCheckdata[props.action].state} text={regexCheckdata[props.action].title} className="regexText" />
+        </RegexComponent>
+    )
+}
