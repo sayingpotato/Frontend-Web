180699fa040b83fd2f45b5a542b784ba93ddf28f
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-29 00:52:46 +0900

    feat : add organisms/registerContentInfo/style.js (#94) 회원 정보 입력 컴포넌트 생성

---
+++ components/organisms/registerContentInfo/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const InfoDiv = styled.div`
+    padding-left: 16px;
+    padding-right : 16px;
+    position: relative;
+`
+
+const AuthDiv = styled.div`
+    display: flex ;
+    align-items: center;
+`;
+
+const RegexDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0 32px;
+`;
+
+
+export { InfoDiv, AuthDiv, RegexDiv }
