10966d01f3a168d890a76006725cfcbbf5b4acfd
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-29 00:52:37 +0900

    feat : add organisms/registerContentInfo/index.js (#94) 회원 정보 입력 컴포넌트 생성

---
+++ components/organisms/registerContentInfo/index.js
@@ -0,0 +1,39 @@
+import { InfoDiv, AuthDiv, RegexDiv } from './style';
+
+import Text from '@atoms/text'
+import Input from '@atoms/input';
+import Image from '@atoms/image'
+import Regex from '@organisms/regex';
+
+const RegisterContentInfo = ({ text, inputOnChange, state, regexData, inputValue, onValidityChange }) => {
+
+    const chooseImage = () => {
+        switch (state) {
+            case 0 : return "/images/emptyBlock.svg"
+            case 1 : return "/images/close.svg"
+            case 2 : return "/images/checkCircle.svg"
+        }
+    }
+
+    return (
+        <InfoDiv>
+            <Text text={text} className="registerTitle" />
+            <Input state={state} onChange={inputOnChange} className="registerInput" />
+            <Image className="close" src={chooseImage()} alt="close" />
+            <RegexDiv>
+                {regexData.map((item, index) => {
+                    return (
+                        <Regex
+                            key={index}
+                            action={item}
+                            input={inputValue}
+                            onValidityChange={onValidityChange}
+                        />
+                    )
+                })}
+            </RegexDiv>
+        </InfoDiv>
+    )
+}
+
+export default RegisterContentInfo
