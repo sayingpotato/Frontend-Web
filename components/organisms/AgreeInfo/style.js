eb0003860584bf5675ea4109eada48464b1c564c
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-28 23:55:27 +0900

    feat : add organisms/AgreeInfo/style.js (#94) 동의 항목 컴포넌트 생성

---
+++ components/organisms/AgreeInfo/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const InfoDiv = styled.div`
+    padding-left: 16px;
+    padding-right : 16px;
+    position: relative;
+`
+
+const AuthDiv = styled.div`
+    display: flex ;
+    align-items: center;
+`;
+
+export { InfoDiv, AuthDiv }
