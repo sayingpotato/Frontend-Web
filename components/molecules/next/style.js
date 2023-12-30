d24249e5c0a9e625d2fe58816d9dc07e36c7bbde
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-30 16:26:37 +0900

    chore : move molecules/next/style.js (#94) component에 존재하던 것을 atomic design 으로 구성

---
+++ components/molecules/next/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const NextComponent = styled.div`
+    position: sticky;
+    text-align: center;
+    padding: 16px 0 0 0;
+    margin-top: 4px;
+    bottom: 0;
+    height: 40px;
+    width: 100vw;
+    font-weight: 700;
+    color: white;
+    background-color: ${props => props.state === 2? '#28469C' : '#B0B0B0'};
+`;
+
+export {NextComponent};
