86f0ab3875a4bdf307641ea5f48644aff617a6c5
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-12-30 20:17:32 +0900

    refactor : remove label/index.js (#94) atomic design 도입으로 인한 불필요한 컴포넌트

--- components/label/index.js
+++
@@ -1,8 +0,0 @@
-import { LabelComponent } from "./style";
-
-export default function Label({title, onClick}){
-    return (
-        <LabelComponent onClick={onClick}>
-            {title}</LabelComponent>
-    )
-}
